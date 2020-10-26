72b5491667b58f5f5720f3d5dad0bae62796eccd
Jonas Bergner <jonas.bergner96@gmail.com>
2020-10-26 16:00:44 +0100

    first commit

---
+++ main_test.go
@@ -0,0 +1,13 @@
+package statefactory
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		assert.Equal(t, 0, 0)
+	})
+}
