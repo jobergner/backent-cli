04d99d89feb7538225c920198c22ef37072b79c4
Java-Jonas <jonas.bergner96@gmail.com>
2020-10-31 19:53:15 +0100

    refactor tests

--- main_test.go
+++ main_test.go
@@ -16,7 +16,8 @@ import (
 type stateMachine ast.File
 
 // "ab c  de\nf" => "ab c de f"
-func normalizeWhitespace(str string) string {
+func normalizeWhitespace(_str string) string {
+	str := strings.TrimSpace(_str)
 	var b strings.Builder
 	b.Grow(len(str))
 
@@ -45,21 +46,151 @@ func normalizeWhitespace(str string) string {
 	return b.String()
 }
 
-func unsafePrintFile(file *stateMachine) string {
+func findDeclarationIn(val string, slice []string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func removeDeclarationFromSlice(slice []string, index int) []string {
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func normalizeDeclarations(declarations []string) []string {
+	var normalizedDeclarations []string
+	for _, def := range declarations {
+		normalizedDeclarations = append(normalizedDeclarations, normalizeWhitespace(def))
+	}
+	return normalizedDeclarations
+}
+
+func matchDeclarations(actualDeclarations, expectedDeclarations []string) (leftoverDeclarations, redundantDeclarations []string) {
+	// redefine redunantDeclarations so it never returns as nil (which happens when there are no redunant strings)
+	// so comparing string slices becomes more conventient
+	redundantDeclarations = make([]string, 0)
+	leftoverDeclarations = make([]string, len(expectedDeclarations))
+	copy(leftoverDeclarations, expectedDeclarations)
+
+	actualDeclarations = normalizeDeclarations(actualDeclarations)
+	leftoverDeclarations = normalizeDeclarations(leftoverDeclarations)
+
+	for _, actualDeclaration := range actualDeclarations {
+		leftoverDeclarationIndex, isFound := findDeclarationIn(actualDeclaration, leftoverDeclarations)
+		if isFound {
+			leftoverDeclarations = removeDeclarationFromSlice(leftoverDeclarations, leftoverDeclarationIndex)
+		} else {
+			redundantDeclarations = append(redundantDeclarations, actualDeclaration)
+		}
+	}
+
+	return leftoverDeclarations, redundantDeclarations
+}
+
+func TestMatchDeclarations(t *testing.T) {
+	t.Run("should ignore leading/trailing whitespace", func(t *testing.T) {
+		actualDeclarations := []string{
+			`   
+
+			type foo struct {
+				a string
+			}
+
+			  `,
+		}
+		expectedDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+		}
+
+		missingDeclarations, redundantDeclarations := matchDeclarations(actualDeclarations, expectedDeclarations)
+
+		assert.Equal(t, 0, len(missingDeclarations))
+		assert.Equal(t, 0, len(redundantDeclarations))
+	})
+	t.Run("should match all declarations", func(t *testing.T) {
+		actualDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+		}
+		expectedDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+		}
+
+		missingDeclarations, redundantDeclarations := matchDeclarations(actualDeclarations, expectedDeclarations)
+
+		assert.Equal(t, 0, len(missingDeclarations))
+		assert.Equal(t, 0, len(redundantDeclarations))
+	})
+	t.Run("should find redundant declaration", func(t *testing.T) {
+		actualDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+			`func (a int) int {
+				return 1+a
+			}`,
+		}
+		expectedDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+		}
+
+		missingDeclarations, redundantDeclarations := matchDeclarations(actualDeclarations, expectedDeclarations)
+
+		assert.Equal(t, 0, len(missingDeclarations))
+		assert.Equal(t, 1, len(redundantDeclarations))
+		assert.Equal(t, []string{normalizeWhitespace(`func (a int) int { return 1+a }`)}, redundantDeclarations)
+	})
+	t.Run("should find missing declaration", func(t *testing.T) {
+		actualDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+		}
+		expectedDeclarations := []string{
+			`type foo struct {
+				a string
+			}`,
+			`func (a int) int {
+				return 1+a
+			}`,
+		}
+
+		missingDeclarations, redundantDeclarations := matchDeclarations(actualDeclarations, expectedDeclarations)
+
+		assert.Equal(t, 1, len(missingDeclarations))
+		assert.Equal(t, 0, len(redundantDeclarations))
+		assert.Equal(t, []string{normalizeWhitespace(`func (a int) int { return 1+a }`)}, missingDeclarations)
+	})
+}
+
+func splitPrintedDeclarations(sm *stateMachine) []string {
+	printedDeclarations := make([]string, 0)
+	for _, decl := range sm.Decls {
+		printedDeclarations = append(printedDeclarations, unsafePrintDeclaration(decl))
+	}
+	return printedDeclarations
+}
+
+func unsafePrintDeclaration(decl ast.Decl) string {
 	var buf bytes.Buffer
-	err := printer.Fprint(&buf, token.NewFileSet(), file)
+	err := printer.Fprint(&buf, token.NewFileSet(), decl)
 	if err != nil {
 		panic(err)
 	}
 	return buf.String()
 }
 
-func stringifyFile(sm *stateMachine) string {
-	return normalizeWhitespace(unsafePrintFile(sm))
-}
-
-func unsafeParseDecls(decls string) *stateMachine {
-	file, err := parser.ParseFile(token.NewFileSet(), "", "package foo\n"+decls, 0)
+func unsafeParseDecls(decls []string) *stateMachine {
+	file, err := parser.ParseFile(token.NewFileSet(), "", "package foo\n"+strings.Join(decls, "\n"), 0)
 	if err != nil {
 		panic(err)
 	}
@@ -69,27 +200,31 @@ func unsafeParseDecls(decls string) *stateMachine {
 
 func TestFlattenStructTree(t *testing.T) {
 	t.Run("should replace object references with ids", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		inputStateMachine := stateMachine(*input)
-		actual := inputStateMachine.flattenStructTree()
-		expected := unsafeParseDecls(`
+		actual := splitPrintedDeclarations(input.flattenStructTree())
+		expected := []string{`
 type person struct {
 	name nameID
-}
+}`, `
 type name struct {
 	first string
 	last string
-} `)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
@@ -99,29 +234,34 @@ func (sm *stateMachine) flattenStructTree() *stateMachine {
 
 func TestAddIDTypes(t *testing.T) {
 	t.Run("should replace object references with ids", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		inputStateMachine := stateMachine(*input)
-		actual := inputStateMachine.addIdTypes()
-		expected := unsafeParseDecls(`
-type personID string
-type nameID string
-type person struct {
-	name nameID
-}
+		actual := splitPrintedDeclarations(input.addIdTypes())
+		expected := []string{
+			`type personID string`,
+			`type nameID string`,
+			`
+type person struct{
+	name name
+}`, `
 type name struct {
 	first string
 	last string
-} `)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
@@ -131,22 +271,24 @@ func (sm *stateMachine) addIdTypes() *stateMachine {
 
 func TestExtractDeclaredStructNames(t *testing.T) {
 	t.Run("should find all struct names in file", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		actual := input.extractDeclaredStructNames()
+		actual := splitPrintedDeclarations(input.extractDeclaredStructNames())
 		expected := []string{"person", "name"}
 
 		assert.Equal(t, expected, actual)
 	})
 }
 
+// TODO not method
 func (sm *stateMachine) extractDeclaredStructNames() *stateMachine {
 	return sm
 }
@@ -158,28 +300,33 @@ type metaField struct {
 
 func TestEmbedStructMetaFields(t *testing.T) {
 	t.Run("should embed meta fields in all structs", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		actual := input.embedStructMetaFields([]metaField{{"lastModified", "int"}})
-		expected := unsafeParseDecls(`
+		actual := splitPrintedDeclarations(input.embedStructMetaFields([]metaField{{"lastModified", "int"}}))
+		expected := []string{`
 type person struct {
 	name name
 	lastModified int
-}
+}`, `
 type name struct {
 	first string
 	last string
 	lastModified int
-}`)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
@@ -189,33 +336,38 @@ func (sm *stateMachine) embedStructMetaFields(metaFields []metaField) *stateMach
 
 func TestEmbedParentageDeclaration(t *testing.T) {
 	t.Run("should add parentage declaration", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		actual := input.embedParentageDeclaration()
-		expected := unsafeParseDecls(`
+		actual := splitPrintedDeclarations(input.embedParentageDeclaration())
+		expected := []string{`
 type person struct {
 	name name
 	parentage Parentage
-}
+}`, `
 type name struct {
 	first string
 	last string
 	parentage Parentage
-}
-type parentage []parentInfo
+}`,
+			`type parentage []parentInfo`, `
 type parentInfo struct {
 	kind entityKind
 	id string
-}`)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
@@ -225,36 +377,40 @@ func (sm *stateMachine) embedParentageDeclaration() *stateMachine {
 
 func TestAddStateMachine(t *testing.T) {
 	t.Run("should add state machine declaration", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		actual := input.addStateMachineDeclaration()
-		expected := unsafeParseDecls(`
+		actual := splitPrintedDeclarations(input.addStateMachineDeclaration())
+		expected := []string{`
 type person struct {
 	name name
-}
+}`, `
 type name struct {
 	first string
 	last string
-}
+}`, `
 type state struct {
 	person map[personID]person
 	name map[nameID]name
-}
+}`, `
 type stateMachine struct {
 	state state
 	patch state
 	patchReceiver chan state
-}
-`)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
@@ -264,28 +420,29 @@ func (sm *stateMachine) addStateMachineDeclaration() *stateMachine {
 
 func TestAddGetters(t *testing.T) {
 	t.Run("adds getters", func(t *testing.T) {
-		input := unsafeParseDecls(`
+		input := unsafeParseDecls([]string{`
 type person struct {
 	name nameID
 	age int
-}
+}`, `
 type name struct {
 	first string
 	last string
-}`)
+}`,
+		})
 
-		actual := input.addGetters([]metaField{{"lastModified", "int"}})
-		expected := unsafeParseDecls(`
+		actual := splitPrintedDeclarations(input.addGetters([]metaField{{"lastModified", "int"}}))
+		expected := []string{`
 type person struct {
 	name nameID
 	age int
 	lastModified int
-}
+}`, `
 type name struct {
 	first string
 	last string
 	lastModified int
-}
+}`, `
 func (sm *stateMachine) GetPerson(personID personID) person {
 	person, ok := sm.patch.person[personID]
 	if ok {
@@ -295,7 +452,7 @@ func (sm *stateMachine) GetPerson(personID personID) person {
 	personCopy := person{}
 	copier.Copy(&personCopy, &currentPerson)
 	return personCopy
-}
+}`, `
 func (sm *stateMachine) GetName(nameID nameID) name {
 	name, ok := sm.patch.name[nameID]
 	if ok {
@@ -305,7 +462,7 @@ func (sm *stateMachine) GetName(nameID nameID) name {
 	nameCopy := name{}
 	copier.Copy(&nameCopy, &currentName)
 	return nameCopy
-}
+}`, `
 func (p person) GetName(sm *stateMachine) name {
 	name, ok := sm.patch.name[p.name]
 	if ok {
@@ -315,19 +472,22 @@ func (p person) GetName(sm *stateMachine) name {
 	nameCopy := name{}
 	copier.Copy(&nameCopy, &currentName)
 	return nameCopy
-}
+}`, `
 func (p person) GetAge() int {
 	return p.age
-}
+}`, `
 func (n name) GetFirst() string {
 	return n.first
-}
+}`, `
 func (n name) GetLast() string {
 	return n.last
-}
-`)
+}`,
+		}
 
-		assert.Equal(t, stringifyFile(expected), stringifyFile(actual))
+		missingDeclarations, redundantDeclarations := matchDeclarations(actual, expected)
+
+		assert.Equal(t, []string{}, missingDeclarations)
+		assert.Equal(t, []string{}, redundantDeclarations)
 	})
 }
 
