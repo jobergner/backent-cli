34bce1ab111a492903e0187ced7ac961149abc3e
Java-Jonas <jonas.bergner96@gmail.com>
2020-11-01 17:34:49 +0100

    consideration of lastModified in setter test

--- add_setters_test.go
+++ add_setters_test.go
@@ -13,64 +13,49 @@ type person struct {
 	id string
 	name nameID
 	age int
-	lastModified int
+	lastModified int64
 }`, `
 type name struct {
 	id string
 	first string
 	last string
-	lastModified int
+	lastModified int64
 }`,
 		})
 
-		metaFields := []metaField{{"lastModified", "int"}, {"id", "string"}}
+		metaFields := []metaField{{"lastModified", "int64"}, {"id", "string"}}
 		actual := splitPrintedDeclarations(input.addSetters(metaFields))
 		expected := []string{`
 type person struct {
 	id string
 	name nameID
 	age int
-	lastModified int
+	lastModified int64
 }`, `
 type name struct {
 	id string
 	first string
 	last string
-	lastModified int
-}`, `
-func (p *person) prepareModification(sm *stateMachine) *person {
-	_, ok := sm.patch.person[p.id]
-	if ok {
-		return p
-	}
-	sm.patch.person[p.id] = *p
-	patchingPerson := &sm.patch.person[p.id] 
-	return patchingPerson
-}`, `
-func (n *name) prepareModification(sm *stateMachine) *name {
-	_, ok := sm.patch.name[n.id]
-	if ok {
-		return n
-	}
-	sm.patch.name[n.id] = *n
-	patchingName := &sm.patch.name[n.id] 
-	return patchingName
+	lastModified int64
 }`, `
 func (p person) SetAge(val int, sm *stateMachine) person {
 	patchingPerson := sm.patch.person[p.id]
 	patchingPerson.age = val
+	patchingPerson.lastModified = time.Now().UnixNano()
 	sm.patch.person[p.id] = patchingPerson
 	return patchingPerson
 }`, `
 func (n name) SetFirst(val string, sm *stateMachine) name {
 	patchingName := sm.patch.name[p.id]
 	patchingName.first = val
+	patchingName.lastModified = time.Now().UnixNano()
 	sm.patch.name[p.id] = patchingName
 	return patchingName
 }`, `
 func (n name) SetLast(val string, sm *stateMachine) name {
 	patchingName := sm.patch.name[p.id]
 	patchingName.last = val
+	patchingName.lastModified = time.Now().UnixNano()
 	sm.patch.name[p.id] = patchingName
 	return patchingName
 }`,
@@ -81,15 +66,6 @@ func (n name) SetLast(val string, sm *stateMachine) name {
 		assert.Equal(t, []string{}, missingDeclarations)
 		assert.Equal(t, []string{}, redundantDeclarations)
 	})
-}
-
-type x int
-
-func (x *x) set() *x {
-	return x
-}
-func (x x) get() x {
-	return x
 }
 
 func (sm *stateMachine) addSetters(metaFields []metaField) *stateMachine {
