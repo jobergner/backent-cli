6c5925d5f12468a50e5c1c0a1611466b2112b140
Java-Jonas <jonas.bergner96@gmail.com>
2020-11-01 18:04:23 +0100

    consider operationKind

--- add_setters_test.go
+++ add_setters_test.go
@@ -13,17 +13,18 @@ type person struct {
 	id string
 	name nameID
 	age int
-	lastModified int64
+	operationKind operationKind
 }`, `
 type name struct {
 	id string
 	first string
 	last string
 	lastModified int64
+	operationKind operationKind
 }`,
 		})
 
-		metaFields := []metaField{{"lastModified", "int64"}, {"id", "string"}}
+		metaFields := []metaField{{"lastModified", "int64"}, {"id", "string"}, {"operationKind", "operationKind"}}
 		actual := splitPrintedDeclarations(input.addSetters(metaFields))
 		expected := []string{`
 type person struct {
@@ -42,6 +43,7 @@ func (p person) SetAge(val int, sm *stateMachine) person {
 	patchingPerson := sm.patch.person[p.id]
 	patchingPerson.age = val
 	patchingPerson.lastModified = time.Now().UnixNano()
+	patchingPerson.operationKind = operationKindUpdate
 	sm.patch.person[p.id] = patchingPerson
 	return patchingPerson
 }`, `
@@ -49,6 +51,7 @@ func (n name) SetFirst(val string, sm *stateMachine) name {
 	patchingName := sm.patch.name[p.id]
 	patchingName.first = val
 	patchingName.lastModified = time.Now().UnixNano()
+	patchingName.operationKind = operationKindUpdate
 	sm.patch.name[p.id] = patchingName
 	return patchingName
 }`, `
@@ -56,6 +59,7 @@ func (n name) SetLast(val string, sm *stateMachine) name {
 	patchingName := sm.patch.name[p.id]
 	patchingName.last = val
 	patchingName.lastModified = time.Now().UnixNano()
+	patchingName.operationKind = operationKindUpdate
 	sm.patch.name[p.id] = patchingName
 	return patchingName
 }`,
