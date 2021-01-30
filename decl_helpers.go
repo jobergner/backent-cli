ea3a32dfed174098c6cbcb9e5ba34c91298f9f24
Java-Jonas <jonas.bergner96@gmail.com>
2021-01-30 17:28:13 +0100

    restucture files

---
+++ decl_helpers.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"go/ast"
+	"go/printer"
+	"go/token"
+	"strings"
+)
+
+func evalDeclName(decl ast.Decl, containingFileName string) string {
+	if isImportDecl(decl) {
+		return evalImportName(containingFileName) + "_import"
+	}
+	if isFuncDecl(decl) {
+		return getFuncName(decl.(*ast.FuncDecl)) + "_func"
+	}
+	if isGenDecl(decl) {
+		return getGenDeclName(decl.(*ast.GenDecl)) + "_type"
+	}
+	panic("unknown decl kind")
+}
+
+func stringifyDecl(decl ast.Decl) string {
+	var buf bytes.Buffer
+	printer.Fprint(&buf, token.NewFileSet(), decl)
+	return buf.String()
+}
+
+func isImportDecl(decl ast.Decl) bool {
+	if genDecl, ok := decl.(*ast.GenDecl); ok {
+		if genDecl.Tok == token.IMPORT {
+			return true
+		}
+	}
+	return false
+}
+
+func isFuncDecl(decl ast.Decl) bool {
+	if _, ok := decl.(*ast.FuncDecl); ok {
+		return true
+	}
+	return false
+}
+
+func isGenDecl(decl ast.Decl) bool {
+	if _, ok := decl.(*ast.GenDecl); ok {
+		return true
+	}
+	return false
+}
+
+// file name "foo.bar" returns "foo_bar"
+func evalImportName(containingFileName string) string {
+	return strings.Replace(containingFileName, ".", "_", -1)
+}
+
+func getFuncName(decl *ast.FuncDecl) string {
+	if decl.Recv == nil {
+		return decl.Name.Name
+	}
+	receiverName := findMethodReceiverIdent(decl.Recv.List[0].Type)
+	return decl.Name.Name + "_" + receiverName.Name
+}
+
+func findMethodReceiverIdent(expr ast.Expr) *ast.Ident {
+	if identType, ok := expr.(*ast.Ident); ok {
+		return identType
+	}
+	if arrayType, ok := expr.(*ast.ArrayType); ok {
+		return findMethodReceiverIdent(arrayType.Elt)
+	}
+	return findMethodReceiverIdent(expr.(*ast.StarExpr).X)
+}
+
+func getGenDeclName(decl *ast.GenDecl) string {
+	if typeSpec, ok := decl.Specs[0].(*ast.TypeSpec); ok {
+		return typeSpec.Name.Name
+	}
+	return decl.Specs[0].(*ast.ValueSpec).Names[0].Name
+}
