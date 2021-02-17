98647842306eaa336ce96a31716deeb1767c2bc7
Java-Jonas <jonas.bergner96@gmail.com>
2021-02-17 22:32:52 +0100

    rework statemachine

--- state_machine_example/deleters.go
+++ state_machine_example/deleters.go
@@ -1,20 +1,48 @@
 package statemachine
 
-// deep deleting
 func (sm *stateMachine) DeletePlayer(playerID playerID) {
-	element := sm.GetPlayer(playerID)
-	element.operationKind = operationKindDelete
-	sm.patch.player[element.id] = element
+	player := sm.GetPlayer(playerID)
+	player.operationKind = operationKindDelete
+	sm.patch.player[player.id] = player
+	for _, itemID := range player.items {
+		sm.DeleteItem(itemID)
+	}
+	sm.DeleteGearScore(player.gearScore)
+	sm.DeletePosition(player.position)
 }
 
-func (sm *stateMachine) DeleteChild(childID childID) {
-	child := sm.GetChild(childID)
-	child.operationKind = operationKindDelete
-	sm.patch.child[child.id] = child
+func (sm *stateMachine) DeleteGearScore(gearScoreID gearScoreID) {
+	gearScore := sm.GetGearScore(gearScoreID)
+	gearScore.operationKind = operationKindDelete
+	sm.patch.gearScore[gearScore.id] = gearScore
 }
 
-func (sm *stateMachine) DeleteName(nameID nameID) {
-	name := sm.GetName(nameID)
-	name.operationKind = operationKindDelete
-	sm.patch.name[name.id] = name
+func (sm *stateMachine) DeletePosition(positionID positionID) {
+	position := sm.GetPosition(positionID)
+	position.operationKind = operationKindDelete
+	sm.patch.position[position.id] = position
+}
+
+func (sm *stateMachine) DeleteItem(itemID itemID) {
+	item := sm.GetItem(itemID)
+	item.operationKind = operationKindDelete
+	sm.patch.item[item.id] = item
+}
+
+func (sm *stateMachine) DeleteZoneItem(zoneItemID zoneItemID) {
+	zoneItem := sm.GetZoneItem(zoneItemID)
+	zoneItem.operationKind = operationKindDelete
+	sm.patch.zoneItem[zoneItem.id] = zoneItem
+}
+
+func (sm *stateMachine) DeleteZone(zoneID zoneID) {
+	zone := sm.GetZone(zoneID)
+	zone.operationKind = operationKindDelete
+	sm.patch.zone[zone.id] = zone
+	for _, playerID := range zone.players {
+		sm.DeletePlayer(playerID)
+	}
+	for _, zoneItemID := range zone.items {
+		sm.DeleteZoneItem(zoneItemID)
+	}
 }
--- state_machine_example/setters.go
+++ state_machine_example/setters.go
@@ -1,22 +1,29 @@
 package statemachine
 
-func (p person) SetAge(val int, sm *stateMachine) person {
-	p.age = val
+func (g gearScore) SetLevel(newLevel int, sm *stateMachine) gearScore {
+	g.level = newLevel
+	g.operationKind = operationKindUpdate
+	sm.patch.gearScore[g.id] = g
+	return g
+}
+
+func (g gearScore) SetScore(newScore int, sm *stateMachine) gearScore {
+	g.score = newScore
+	g.operationKind = operationKindUpdate
+	sm.patch.gearScore[g.id] = g
+	return g
+}
+
+func (p position) SetX(newX float64, sm *stateMachine) position {
+	p.x = newX
 	p.operationKind = operationKindUpdate
-	sm.patch.person[p.id] = p
+	sm.patch.position[p.id] = p
 	return p
 }
 
-func (n name) SetFirst(val string, sm *stateMachine) name {
-	n.first = val
-	n.operationKind = operationKindUpdate
-	sm.patch.name[n.id] = n
-	return n
-}
-
-func (n name) SetLast(val string, sm *stateMachine) name {
-	n.last = val
-	n.operationKind = operationKindUpdate
-	sm.patch.name[n.id] = n
-	return n
+func (p position) SetY(newY float64, sm *stateMachine) position {
+	p.x = newY
+	p.operationKind = operationKindUpdate
+	sm.patch.position[p.id] = p
+	return p
 }
