f2a5d9bb23fb354982b8849bba21d872963054f6
Jonas Bergner <jonas.bergner96@gmail.com>
2021-03-06 10:55:28 +0100

    write adders

---
+++ write_adders.go
@@ -0,0 +1,29 @@
+package statefactory
+
+import (
+	"text/template"
+)
+
+const adderTemplateString string = `
+<( range .Decls )><( $Decl := . )><( range .Fields )><( if .HasSliceValue )>
+func (_e <( toTitleCase $Decl.Name )>) Add<(if .ValueType.IsBasicType )><( toTitleCase .Name )><( else )><( toTitleCase .ValueType.Name )><( end )>(sm *StateMachine<(if .ValueType.IsBasicType )>, <( .Name)> ...<( .ValueType.Name )><( end )>) <( if not .ValueType.IsBasicType )><( toTitleCase .ValueType.Name )><( end )> {
+	e := sm.Get<( toTitleCase $Decl.Name )>(_e.<( $Decl.Name )>.ID)
+	if e.<( $Decl.Name )>.OperationKind == OperationKindDelete {
+		return<( if not .ValueType.IsBasicType )> <( toTitleCase .ValueType.Name )>{}<( end )>
+	}
+	<( if not .ValueType.IsBasicType )><( .ValueType.Name )> := sm.create<( toTitleCase .ValueType.Name )>(true)
+	<( end )>e.<( $Decl.Name )>.<( toTitleCase .Name )> = append(e.<( $Decl.Name )>.<( toTitleCase .Name )>, <(if .ValueType.IsBasicType )><( .Name )>...<( else )><( .ValueType.Name )>.<( .ValueType.Name )>.ID<( end )>)
+	e.<( $Decl.Name )>.OperationKind = OperationKindUpdate
+	sm.Patch.<( toTitleCase $Decl.Name )>[e.<( $Decl.Name )>.ID] = e.<( $Decl.Name )><( if not .ValueType.IsBasicType )>
+	return <( .ValueType.Name )><( end )>
+}
+<( end )>
+<( end )><( end )>
+`
+
+var adderTemplate *template.Template = newTemplateFrom("adderTemplate", adderTemplateString)
+
+func (s *stateFactory) writeAdders() *stateFactory {
+	adderTemplate.Execute(&s.buf, s.ast)
+	return s
+}
