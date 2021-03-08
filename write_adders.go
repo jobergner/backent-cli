c814d0d89c135ee2f1e8c7702c6bbb40a248587b
Java-Jonas <jonas.bergner96@gmail.com>
2021-03-08 17:41:48 +0100

    tidy up templates

--- write_adders.go
+++ write_adders.go
@@ -6,13 +6,25 @@ import (
 
 const adderTemplateString string = `
 <( range .Decls )><( $Decl := . )><( range .Fields )><( if .HasSliceValue )>
-func (_e <( toTitleCase $Decl.Name )>) Add<(if .ValueType.IsBasicType )><( toTitleCase .Name )><( else )><( toTitleCase .ValueType.Name )><( end )>(sm *StateMachine<(if .ValueType.IsBasicType )>, <( .Name)> ...<( .ValueType.Name )><( end )>) <( if not .ValueType.IsBasicType )><( toTitleCase .ValueType.Name )><( end )> {
+func (_e <( toTitleCase $Decl.Name )>) Add
+<(- if .ValueType.IsBasicType -)>
+	<( toTitleCase .Name )>
+<(- else -)>
+	<( toTitleCase .ValueType.Name )>
+<(- end -)>
+(sm *StateMachine<(if .ValueType.IsBasicType )>, <( .Name)> ...<( .ValueType.Name )><( end )>) <( if not .ValueType.IsBasicType )><( toTitleCase .ValueType.Name )><( end )> {
 	e := sm.Get<( toTitleCase $Decl.Name )>(_e.<( $Decl.Name )>.ID)
 	if e.<( $Decl.Name )>.OperationKind == OperationKindDelete {
 		return<( if not .ValueType.IsBasicType )> <( toTitleCase .ValueType.Name )>{}<( end )>
 	}
 	<( if not .ValueType.IsBasicType )><( .ValueType.Name )> := sm.create<( toTitleCase .ValueType.Name )>(true)
-	<( end )>e.<( $Decl.Name )>.<( toTitleCase .Name )> = append(e.<( $Decl.Name )>.<( toTitleCase .Name )>, <(if .ValueType.IsBasicType )><( .Name )>...<( else )><( .ValueType.Name )>.<( .ValueType.Name )>.ID<( end )>)
+	<( end )>e.<( $Decl.Name )>.<( toTitleCase .Name )> = append(e.<( $Decl.Name )>.<( toTitleCase .Name )>,<(print " ")>
+	<(- if .ValueType.IsBasicType -)>
+		<( .Name )>...
+	<(- else -)>
+		<( .ValueType.Name )>.<( .ValueType.Name )>.ID
+	<(- end -)>
+	)
 	e.<( $Decl.Name )>.OperationKind = OperationKindUpdate
 	sm.Patch.<( toTitleCase $Decl.Name )>[e.<( $Decl.Name )>.ID] = e.<( $Decl.Name )><( if not .ValueType.IsBasicType )>
 	return <( .ValueType.Name )><( end )>
