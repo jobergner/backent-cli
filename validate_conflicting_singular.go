31d413ca3d9f381124675497ea3bbd5209f93913
Jonas Bergner <jonas.bergner96@gmail.com>
2021-03-12 19:31:05 +0100

    conflicting singular validation

---
+++ validate_conflicting_singular.go
@@ -0,0 +1,41 @@
+package validator
+
+import (
+	"fmt"
+	"github.com/gertd/go-pluralize"
+)
+
+var pluralizeClient *pluralize.Client = pluralize.NewClient()
+
+func validateConflictingSingular(data map[interface{}]interface{}) (errs []error) {
+
+	for _, value := range data {
+		if isMap(value) {
+			mapValue := value.(map[interface{}]interface{})
+			objectValidationErrs := validateConflictingSingularObject(mapValue)
+			errs = append(errs, objectValidationErrs...)
+		}
+	}
+
+	return
+}
+
+func validateConflictingSingularObject(
+	yamlObjectData map[interface{}]interface{},
+) (errs []error) {
+
+	for key := range yamlObjectData {
+		keyName := fmt.Sprintf("%v", key)
+		if pluralizeClient.IsPlural(keyName) {
+			for _key := range yamlObjectData {
+				_keyName := fmt.Sprintf("%v", _key)
+				singularForm := pluralizeClient.Singular(keyName)
+				if singularForm == _keyName {
+					errs = append(errs, newValidationErrorConflictingSingular(_keyName, keyName, singularForm))
+				}
+			}
+		}
+	}
+
+	return
+}
