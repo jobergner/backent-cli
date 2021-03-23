1aa8b7222ec53323916cdfc901455882654797ba
Java-Jonas <jonas.bergner96@gmail.com>
2021-03-23 21:58:56 +0100

    refactor helpers

--- enginefactory/write_helpers.go
+++ enginefactory/write_helpers.go
@@ -1,33 +1,70 @@
 package enginefactory
 
 import (
-	"text/template"
+	. "github.com/dave/jennifer/jen"
 )
 
-const deduplicateTemplateString string = `
-<( range .Types )>
-func deduplicate<( toTitleCase .Name )>IDs(a []<( toTitleCase .Name )>ID, b []<( toTitleCase .Name )>ID) []<( toTitleCase .Name )>ID {
-
-	check := make(map[<( toTitleCase .Name )>ID]bool)
-	deduped := make([]<( toTitleCase .Name )>ID, 0)
-	for _, val := range a {
-		check[val] = true
-	}
-	for _, val := range b {
-		check[val] = true
-	}
-
-	for val := range check {
-		deduped = append(deduped, val)
-	}
-
-	return deduped
-}<( end )>
-`
-
-var deduplicateTemplate *template.Template = newTemplateFrom("deduplicateTemplate", deduplicateTemplateString)
-
 func (s *stateFactory) writeDeduplicate() *stateFactory {
-	deduplicateTemplate.Execute(s.buf, s.ast)
+	decls := newDeclSet()
+	s.ast.rangeTypes(func(configType stateConfigType) {
+
+		d := deduplicator{
+			t: configType,
+		}
+
+		decls.file.Func().Id(d.name()).Params(d.params()).Id(d.returns()).Block(
+			d.defineCheck(),
+			d.defineDeduped(),
+			d.loopIDs("a"),
+			d.loopIDs("b"),
+			d.loopCheck(),
+			Return(Id("deduped")),
+		)
+	})
+
+	decls.render(s.buf)
 	return s
+}
+
+type deduplicator struct {
+	t stateConfigType
+}
+
+func (d deduplicator) name() string {
+	return "deduplicate" + title(d.t.Name) + "IDs"
+}
+
+func (d deduplicator) idType() string {
+	return title(d.t.Name) + "ID"
+}
+
+func (d deduplicator) params() *Statement {
+	return List(Id("a").Id("[]"+d.idType()), Id("b").Id("[]"+d.idType()))
+}
+
+func (d deduplicator) returns() string {
+	return "[]" + d.idType()
+}
+
+func (d deduplicator) defineCheck() *Statement {
+	return Id("check").Op(":=").Make(Map(Id(d.idType())).Bool())
+}
+
+func (d deduplicator) defineDeduped() *Statement {
+	return Id("deduped").Op(":=").Make(Id(d.returns()), Lit(0))
+
+}
+
+func (d deduplicator) loopIDs(getsLooped string) *Statement {
+	loop := For(List(Id("_"), Id("val")).Op(":=").Range().Id(getsLooped)).Block(
+		Id("check").Index(Id("val")).Op("=").Id("true"),
+	)
+	return loop
+}
+
+func (d deduplicator) loopCheck() *Statement {
+	loop := For(Id("val").Op(":=").Range().Id("check")).Block(
+		Id("deduped").Op("=").Append(Id("deduped"), Id("val")),
+	)
+	return loop
 }
