1084acc6fc8c5b26002593d76292742635aa3dae
Jonas Bergner <jonas.bergner96@gmail.com>
2021-03-26 18:34:17 +0100

    remove unused code

--- enginefactory/write_creators.go
+++ enginefactory/write_creators.go
@@ -2,34 +2,7 @@ package enginefactory
 
 import (
 	. "github.com/dave/jennifer/jen"
-	"text/template"
 )
-
-const creatorTemplateString string = `
-<(- range .Types )>
-func (se *Engine) Create<( toTitleCase .Name )>() <( toTitleCase .Name )> {
-	return se.create<( toTitleCase .Name )>(<( if not .IsRootType )>false<( end )>)
-}
-func (se *Engine) create<( toTitleCase .Name )>(<( if not .IsRootType )>hasParent bool<( end )>) <( toTitleCase .Name )> {
-	var e <( .Name )>Core
-	e.ID = <( toTitleCase .Name )>ID(se.GenerateID())
-	<(- if not .IsRootType )>
-		e.HasParent_ = hasParent
-	<(- end )>
-	<(- range .Fields -)>
-		<(- if not .HasSliceValue )><( if not .ValueType.IsBasicType )>
-			element<( toTitleCase .ValueType.Name )> := se.create<( toTitleCase .ValueType.Name )>(true)
-			e.<( toTitleCase .Name )> = element<( toTitleCase .ValueType.Name )>.<( .ValueType.Name )>.ID
-		<(- end )><( end -)>
-	<( end )>
-	e.OperationKind_ = OperationKindUpdate
-	se.Patch.<( toTitleCase .Name )>[e.ID] = e
-	return <( toTitleCase .Name )>{<( .Name )>: e}
-}
-<( end )>
-`
-
-var creatorTemplate *template.Template = newTemplateFrom("creatorTemplate", creatorTemplateString)
 
 func (s *stateFactory) writeCreators() *stateFactory {
 	decls := newDeclSet()
