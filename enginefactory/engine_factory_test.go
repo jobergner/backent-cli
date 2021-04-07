627c167c3da229780e9d91caf1a54edd63e3da90
Jonas Bergner <jonas.bergner96@gmail.com>
2021-04-07 15:57:07 +0200

    golden file test

--- enginefactory/engine_factory_test.go
+++ enginefactory/engine_factory_test.go
@@ -1,38 +1,55 @@
 package enginefactory
 
 import (
+	"bar-cli/ast"
+	"bar-cli/utils"
+	"io/ioutil"
 	"testing"
 )
 
+var testData = map[interface{}]interface{}{
+	"player": map[interface{}]interface{}{
+		"items":     "[]item",
+		"gearScore": "gearScore",
+		"position":  "position",
+	},
+	"zone": map[interface{}]interface{}{
+		"items":   "[]zoneItem",
+		"players": "[]player",
+		"tags":    "[]string",
+	},
+	"zoneItem": map[interface{}]interface{}{
+		"position": "position",
+		"item":     "item",
+	},
+	"position": map[interface{}]interface{}{
+		"x": "float64",
+		"y": "float64",
+	},
+	"item": map[interface{}]interface{}{
+		"gearScore": "gearScore",
+	},
+	"gearScore": map[interface{}]interface{}{
+		"level": "int",
+		"score": "int",
+	},
+}
+
 func TestStateFactory(t *testing.T) {
-	data := map[interface{}]interface{}{
-		"player": map[interface{}]interface{}{
-			"items":     "[]item",
-			"gearScore": "gearScore",
-			"position":  "position",
-		},
-		"zone": map[interface{}]interface{}{
-			"items":   "[]zoneItem",
-			"players": "[]player",
-			"tags":    "[]string",
-		},
-		"zoneItem": map[interface{}]interface{}{
-			"position": "position",
-			"item":     "item",
-		},
-		"position": map[interface{}]interface{}{
-			"x": "float64",
-			"y": "float64",
-		},
-		"item": map[interface{}]interface{}{
-			"gearScore": "gearScore",
-		},
-		"gearScore": map[interface{}]interface{}{
-			"level": "int",
-			"score": "int",
-		},
-	}
 	t.Run("doesnt crash", func(t *testing.T) {
-		WriteEngineFrom(data)
+		expected, err := ioutil.ReadFile("testdata/golden.go")
+		if err != nil {
+			t.Fatalf("Error loading golden file: %s", err)
+		}
+		actual := WriteEngineFrom(testData)
+
+		if string(expected) != string(actual) {
+			t.Errorf(utils.Diff(string(actual), string(expected)))
+		}
 	})
+}
+
+func newSimpleASTExample() *ast.AST {
+	simpleAST := ast.Parse(testData, map[interface{}]interface{}{})
+	return simpleAST
 }
