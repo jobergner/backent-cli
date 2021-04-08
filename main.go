55e50b0e5b3f3cc314fa718575dc222fbbe3b86c
Jonas Bergner <jonas.bergner96@gmail.com>
2021-04-08 15:49:28 +0200

    minor restructuring

--- main.go
+++ main.go
@@ -2,38 +2,11 @@ package main
 
 import (
 	"bar-cli/enginefactory"
+	"bar-cli/examples"
 	"fmt"
 )
 
 func main() {
-	data := map[interface{}]interface{}{
-		"player": map[interface{}]interface{}{
-			"items":     "[]item",
-			"gearScore": "gearScore",
-			"position":  "position",
-		},
-		"zone": map[interface{}]interface{}{
-			"items":   "[]zoneItem",
-			"players": "[]player",
-			"tags":    "[]string",
-		},
-		"zoneItem": map[interface{}]interface{}{
-			"position": "position",
-			"item":     "item",
-		},
-		"position": map[interface{}]interface{}{
-			"x": "float64",
-			"y": "float64",
-		},
-		"item": map[interface{}]interface{}{
-			"gearScore": "gearScore",
-		},
-		"gearScore": map[interface{}]interface{}{
-			"level": "int",
-			"score": "int",
-		},
-	}
-
-	state := enginefactory.WriteEngineFrom(data)
+	state := enginefactory.WriteEngineFrom(examples.StateConfig)
 	fmt.Println(string(state))
 }
