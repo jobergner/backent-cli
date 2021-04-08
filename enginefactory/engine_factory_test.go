4896d6116f2b511890e3e6be21f27de57222ee2b
Java-Jonas <jonas.bergner96@gmail.com>
2021-04-08 15:49:28 +0200

    minor restructuring

--- enginefactory/engine_factory_test.go
+++ enginefactory/engine_factory_test.go
@@ -2,38 +2,11 @@ package enginefactory
 
 import (
 	"bar-cli/ast"
+	"bar-cli/examples"
 	"bar-cli/utils"
 	"io/ioutil"
 	"testing"
 )
-
-var testData = map[interface{}]interface{}{
-	"player": map[interface{}]interface{}{
-		"items":     "[]item",
-		"gearScore": "gearScore",
-		"position":  "position",
-	},
-	"zone": map[interface{}]interface{}{
-		"items":   "[]zoneItem",
-		"players": "[]player",
-		"tags":    "[]string",
-	},
-	"zoneItem": map[interface{}]interface{}{
-		"position": "position",
-		"item":     "item",
-	},
-	"position": map[interface{}]interface{}{
-		"x": "float64",
-		"y": "float64",
-	},
-	"item": map[interface{}]interface{}{
-		"gearScore": "gearScore",
-	},
-	"gearScore": map[interface{}]interface{}{
-		"level": "int",
-		"score": "int",
-	},
-}
 
 func TestStateFactory(t *testing.T) {
 	t.Run("doesnt crash", func(t *testing.T) {
@@ -41,7 +14,7 @@ func TestStateFactory(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Error loading golden file: %s", err)
 		}
-		actual := WriteEngineFrom(testData)
+		actual := WriteEngineFrom(examples.StateConfig)
 
 		if string(expected) != string(actual) {
 			t.Errorf(utils.Diff(string(actual), string(expected)))
@@ -50,6 +23,6 @@ func TestStateFactory(t *testing.T) {
 }
 
 func newSimpleASTExample() *ast.AST {
-	simpleAST := ast.Parse(testData, map[interface{}]interface{}{})
+	simpleAST := ast.Parse(examples.StateConfig, map[interface{}]interface{}{})
 	return simpleAST
 }
---
+++ examples/data.go
@@ -0,0 +1,29 @@
+package examples
+
+var StateConfig = map[interface{}]interface{}{
+	"player": map[interface{}]interface{}{
+		"items":     "[]item",
+		"gearScore": "gearScore",
+		"position":  "position",
+	},
+	"zone": map[interface{}]interface{}{
+		"items":   "[]zoneItem",
+		"players": "[]player",
+		"tags":    "[]string",
+	},
+	"zoneItem": map[interface{}]interface{}{
+		"position": "position",
+		"item":     "item",
+	},
+	"position": map[interface{}]interface{}{
+		"x": "float64",
+		"y": "float64",
+	},
+	"item": map[interface{}]interface{}{
+		"gearScore": "gearScore",
+	},
+	"gearScore": map[interface{}]interface{}{
+		"level": "int",
+		"score": "int",
+	},
+}
