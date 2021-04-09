0555fdb699b3577ef7d86461c1430d8d6340cfae
Jonas Bergner <jonas.bergner96@gmail.com>
2021-04-09 16:21:29 +0200

    minor restruc

--- serverfactory/server_example/main.go
+++ serverfactory/server_example/main.go
@@ -1,55 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"nhooyr.io/websocket"
+	server "bar-cli/serverfactory/server_example/server"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request, room *Room) {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	websocketConnection, err := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	client, err := newClient(NewConnection(websocketConnection, r))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	client.assignToRoom(room)
-	room.registerChannel <- client
-
-	go client.runReadMessages()
-	go client.runWriteMessages()
-
-	err = client.conn.WriteMessage([]byte("hello"))
-	if err != nil {
-		log.Println(err)
-	}
-
-	// wait until client disconnects
-	<-r.Context().Done()
-}
-
-func setupRoutes() {
-	room := newRoom()
-	room.Deploy()
-
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) { wsEndpoint(w, r, room) })
-}
-
 func main() {
-	log.Println("Hello World")
-	setupRoutes()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server.Start()
 }
