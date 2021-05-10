30b5c99ceb02980965ac655ef25f0e5fc12f3e32
Jonas Bergner <jonas.bergner96@gmail.com>
2021-05-10 17:29:48 +0200

    write mergers

--- enginefactory/write_helpers.go
+++ enginefactory/write_helpers.go
@@ -91,20 +91,20 @@ func (s *EngineFactory) writeAllIDsMethod() *EngineFactory {
 				return
 			}
 
-			d := allIDsMehtodWriter{
+			a := allIDsMehtodWriter{
 				f: field,
 			}
 
-			decls.File.Func().Params(d.receiverParams()).Id(d.name()).Params().Id(d.returns()).Block(
-				d.declareStateIDsSlice(),
-				For(d.stateIDsLoopConditions()).Block(
-					d.appendStateID(),
+			decls.File.Func().Params(a.receiverParams()).Id(a.name()).Params().Id(a.returns()).Block(
+				a.declareStateIDsSlice(),
+				For(a.stateIDsLoopConditions()).Block(
+					a.appendStateID(),
 				),
-				d.declarePatchIDsSlice(),
-				For(d.patchIDsLoopConditions()).Block(
-					d.appendPatchID(),
+				a.declarePatchIDsSlice(),
+				For(a.patchIDsLoopConditions()).Block(
+					a.appendPatchID(),
 				),
-				Return(d.deduplicatedIDs()),
+				Return(a.deduplicatedIDs()),
 			)
 		})
 	})
@@ -117,55 +117,164 @@ type allIDsMehtodWriter struct {
 	f ast.Field
 }
 
-func (d allIDsMehtodWriter) typeName() string {
-	return title(d.f.Parent.Name) + title(pluralizeClient.Singular(d.f.Name)) + "Ref"
+func (a allIDsMehtodWriter) typeName() string {
+	return title(a.f.Parent.Name) + title(pluralizeClient.Singular(a.f.Name)) + "Ref"
 }
 
-func (d allIDsMehtodWriter) idType() string {
-	return d.typeName() + "ID"
+func (a allIDsMehtodWriter) idType() string {
+	return a.typeName() + "ID"
 }
 
-func (d allIDsMehtodWriter) name() string {
-	return "all" + d.idType() + "s"
+func (a allIDsMehtodWriter) name() string {
+	return "all" + a.idType() + "s"
 }
 
-func (d allIDsMehtodWriter) receiverParams() *Statement {
+func (a allIDsMehtodWriter) receiverParams() *Statement {
 	return Id("engine").Id("Engine")
 }
 
-func (d allIDsMehtodWriter) returns() string {
-	return "[]" + d.idType()
+func (a allIDsMehtodWriter) returns() string {
+	return "[]" + a.idType()
 }
 
-func (d allIDsMehtodWriter) idSliceName(prefix string) string {
-	return prefix + d.idType() + "s"
+func (a allIDsMehtodWriter) idSliceName(prefix string) string {
+	return prefix + a.idType() + "s"
 }
 
-func (d allIDsMehtodWriter) declareStateIDsSlice() *Statement {
-	return Var().Id(d.idSliceName("state")).Id("[]" + d.idType())
+func (a allIDsMehtodWriter) declareStateIDsSlice() *Statement {
+	return Var().Id(a.idSliceName("state")).Id("[]" + a.idType())
 }
 
-func (d allIDsMehtodWriter) stateIDsLoopConditions() *Statement {
-	return Id(lower(d.idType())).Op(":=").Range().Id("engine").Dot("State").Dot(d.typeName())
+func (a allIDsMehtodWriter) stateIDsLoopConditions() *Statement {
+	return Id(lower(a.idType())).Op(":=").Range().Id("engine").Dot("State").Dot(a.typeName())
 }
 
-func (d allIDsMehtodWriter) appendStateID() *Statement {
-	return Id(d.idSliceName("state")).Op("=").Append(Id(d.idSliceName("state")), Id(lower(d.idType())))
+func (a allIDsMehtodWriter) appendStateID() *Statement {
+	return Id(a.idSliceName("state")).Op("=").Append(Id(a.idSliceName("state")), Id(lower(a.idType())))
 }
 
-func (d allIDsMehtodWriter) declarePatchIDsSlice() *Statement {
-	return Var().Id(d.idSliceName("patch")).Id("[]" + d.idType())
+func (a allIDsMehtodWriter) declarePatchIDsSlice() *Statement {
+	return Var().Id(a.idSliceName("patch")).Id("[]" + a.idType())
 }
 
-func (d allIDsMehtodWriter) patchIDsLoopConditions() *Statement {
-	return Id(lower(d.idType())).Op(":=").Range().Id("engine").Dot("Patch").Dot(d.typeName())
+func (a allIDsMehtodWriter) patchIDsLoopConditions() *Statement {
+	return Id(lower(a.idType())).Op(":=").Range().Id("engine").Dot("Patch").Dot(a.typeName())
 }
 
-func (d allIDsMehtodWriter) appendPatchID() *Statement {
-	return Id(d.idSliceName("patch")).Op("=").Append(Id(d.idSliceName("patch")), Id(lower(d.idType())))
+func (a allIDsMehtodWriter) appendPatchID() *Statement {
+	return Id(a.idSliceName("patch")).Op("=").Append(Id(a.idSliceName("patch")), Id(lower(a.idType())))
 }
 
-func (d allIDsMehtodWriter) deduplicatedIDs() *Statement {
-	dedu := deduplicateWriter{d.f}
-	return Id(dedu.name()).Call(Id(d.idSliceName("state")), Id(d.idSliceName("patch")))
+func (a allIDsMehtodWriter) deduplicatedIDs() *Statement {
+	dedu := deduplicateWriter{a.f}
+	return Id(dedu.name()).Call(Id(a.idSliceName("state")), Id(a.idSliceName("patch")))
+}
+
+func (s *EngineFactory) writeMergeIDs() *EngineFactory {
+	decls := NewDeclSet()
+	s.config.RangeTypes(func(configType ast.ConfigType) {
+		m := mergeIDsWriter{
+			idType: func() string {
+				return title(configType.Name) + "ID"
+			},
+		}
+
+		decls.File.Func().Id(m.name()).Params(m.params()).Id(m.returns()).Block(
+			m.declareIDs(),
+			m.copyIDs(),
+			m.declareCounter(),
+			For(m.currentIDsLoopConditions()).Block(
+				If(m.idDoesNotMatch()).Block(
+					Continue(),
+				),
+				m.incrementCounter(),
+			),
+			For(m.nextIDsLoopConditions()).Block(
+				m.appendID(),
+			),
+			Return(Id("ids")),
+		)
+
+	})
+	s.config.RangeTypes(func(configType ast.ConfigType) {
+		configType.RangeFields(func(field ast.Field) {
+			if !field.HasPointerValue {
+				return
+			}
+
+			m := mergeIDsWriter{
+				idType: func() string {
+					return title(field.Parent.Name) + title(pluralizeClient.Singular(field.Name)) + "RefID"
+				},
+			}
+
+			decls.File.Func().Id(m.name()).Params(m.params()).Id(m.returns()).Block(
+				m.declareIDs(),
+				m.copyIDs(),
+				m.declareCounter(),
+				For(m.currentIDsLoopConditions()).Block(
+					If(m.idDoesNotMatch()).Block(
+						Continue(),
+					),
+					m.incrementCounter(),
+				),
+				For(m.nextIDsLoopConditions()).Block(
+					m.appendID(),
+				),
+				Return(Id("ids")),
+			)
+
+		})
+	})
+
+	decls.Render(s.buf)
+	return s
+}
+
+type mergeIDsWriter struct {
+	idType func() string
+}
+
+func (m mergeIDsWriter) name() string {
+	return "merge" + m.idType() + "s"
+}
+
+func (m mergeIDsWriter) params() *Statement {
+	return List(Id("currentIDs, nextIDs").Id("[]" + m.idType()))
+}
+
+func (m mergeIDsWriter) returns() string {
+	return "[]" + m.idType()
+}
+
+func (m mergeIDsWriter) declareIDs() *Statement {
+	return Id("ids").Op(":=").Make(Id("[]"+m.idType()), Len(Id("currentIDs")))
+}
+
+func (m mergeIDsWriter) copyIDs() *Statement {
+	return Copy(Id("ids"), Id("currentIDs"))
+}
+
+func (m mergeIDsWriter) declareCounter() *Statement {
+	return Var().Id("j").Int()
+}
+
+func (m mergeIDsWriter) currentIDsLoopConditions() *Statement {
+	return List(Id("_"), Id("currentID")).Op(":=").Range().Id("currentIDs")
+}
+
+func (m mergeIDsWriter) idDoesNotMatch() *Statement {
+	return Len(Id("nextIDs")).Op("<=").Id("j").Op("||").Id("currentID").Op("!=").Id("nextIDs").Index(Id("j"))
+}
+
+func (m mergeIDsWriter) incrementCounter() *Statement {
+	return Id("j").Op("+=").Lit(1)
+}
+
+func (m mergeIDsWriter) nextIDsLoopConditions() *Statement {
+	return List(Id("_"), Id("nextID")).Op(":=").Range().Id("nextIDs").Index(Id("j:"))
+}
+
+func (m mergeIDsWriter) appendID() *Statement {
+	return Id("ids").Op("=").Append(Id("ids"), Id("nextID"))
 }
