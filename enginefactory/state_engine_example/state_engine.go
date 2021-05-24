2889fa9266f1810866514810810cf49b5224fa11
Jonas Bergner <jonas.bergner96@gmail.com>
2021-05-24 21:55:41 +0200

    write engine

--- enginefactory/state_engine_example/state_engine.go
+++ enginefactory/state_engine_example/state_engine.go
@@ -20,194 +20,194 @@ func newEngine() *Engine {
 	return &Engine{
 		IDgen:     1,
 		Patch:     newState(),
-		State:     newState(),
 		PathTrack: newPathTrack(),
+		State:     newState(),
 		Tree:      newTree(),
 	}
 }
 
-func (se *Engine) GenerateID() int {
-	newID := se.IDgen
-	se.IDgen = se.IDgen + 1
+func (engine *Engine) GenerateID() int {
+	newID := engine.IDgen
+	engine.IDgen = engine.IDgen + 1
 	return newID
 }
 
-func (se *Engine) UpdateState() {
-	for _, equipmentSet := range se.Patch.EquipmentSet {
+func (engine *Engine) UpdateState() {
+	for _, equipmentSet := range engine.Patch.EquipmentSet {
 		if equipmentSet.OperationKind == OperationKindDelete {
-			delete(se.State.EquipmentSet, equipmentSet.ID)
+			delete(engine.State.EquipmentSet, equipmentSet.ID)
 		} else {
 			equipmentSet.OperationKind = OperationKindUnchanged
-			se.State.EquipmentSet[equipmentSet.ID] = equipmentSet
+			engine.State.EquipmentSet[equipmentSet.ID] = equipmentSet
 		}
 	}
-	for _, gearScore := range se.Patch.GearScore {
+	for _, gearScore := range engine.Patch.GearScore {
 		if gearScore.OperationKind == OperationKindDelete {
-			delete(se.State.GearScore, gearScore.ID)
+			delete(engine.State.GearScore, gearScore.ID)
 		} else {
 			gearScore.OperationKind = OperationKindUnchanged
-			se.State.GearScore[gearScore.ID] = gearScore
+			engine.State.GearScore[gearScore.ID] = gearScore
 		}
 	}
-	for _, item := range se.Patch.Item {
+	for _, item := range engine.Patch.Item {
 		if item.OperationKind == OperationKindDelete {
-			delete(se.State.Item, item.ID)
+			delete(engine.State.Item, item.ID)
 		} else {
 			item.OperationKind = OperationKindUnchanged
-			se.State.Item[item.ID] = item
+			engine.State.Item[item.ID] = item
 		}
 	}
-	for _, player := range se.Patch.Player {
+	for _, player := range engine.Patch.Player {
 		if player.OperationKind == OperationKindDelete {
-			delete(se.State.Player, player.ID)
+			delete(engine.State.Player, player.ID)
 		} else {
 			player.OperationKind = OperationKindUnchanged
-			se.State.Player[player.ID] = player
+			engine.State.Player[player.ID] = player
 		}
 	}
-	for _, position := range se.Patch.Position {
+	for _, position := range engine.Patch.Position {
 		if position.OperationKind == OperationKindDelete {
-			delete(se.State.Position, position.ID)
+			delete(engine.State.Position, position.ID)
 		} else {
 			position.OperationKind = OperationKindUnchanged
-			se.State.Position[position.ID] = position
+			engine.State.Position[position.ID] = position
 		}
 	}
-	for _, zone := range se.Patch.Zone {
+	for _, zone := range engine.Patch.Zone {
 		if zone.OperationKind == OperationKindDelete {
-			delete(se.State.Zone, zone.ID)
+			delete(engine.State.Zone, zone.ID)
 		} else {
 			zone.OperationKind = OperationKindUnchanged
-			se.State.Zone[zone.ID] = zone
+			engine.State.Zone[zone.ID] = zone
 		}
 	}
-	for _, zoneItem := range se.Patch.ZoneItem {
+	for _, zoneItem := range engine.Patch.ZoneItem {
 		if zoneItem.OperationKind == OperationKindDelete {
-			delete(se.State.ZoneItem, zoneItem.ID)
+			delete(engine.State.ZoneItem, zoneItem.ID)
 		} else {
 			zoneItem.OperationKind = OperationKindUnchanged
-			se.State.ZoneItem[zoneItem.ID] = zoneItem
+			engine.State.ZoneItem[zoneItem.ID] = zoneItem
 		}
 	}
-	for _, equipmentSetEquipmentRef := range se.Patch.EquipmentSetEquipmentRef {
+	for _, equipmentSetEquipmentRef := range engine.Patch.EquipmentSetEquipmentRef {
 		if equipmentSetEquipmentRef.OperationKind == OperationKindDelete {
-			delete(se.State.EquipmentSetEquipmentRef, equipmentSetEquipmentRef.ID)
+			delete(engine.State.EquipmentSetEquipmentRef, equipmentSetEquipmentRef.ID)
 		} else {
 			equipmentSetEquipmentRef.OperationKind = OperationKindUnchanged
-			se.State.EquipmentSetEquipmentRef[equipmentSetEquipmentRef.ID] = equipmentSetEquipmentRef
+			engine.State.EquipmentSetEquipmentRef[equipmentSetEquipmentRef.ID] = equipmentSetEquipmentRef
 		}
 	}
-	for _, itemBoundToRef := range se.Patch.ItemBoundToRef {
+	for _, itemBoundToRef := range engine.Patch.ItemBoundToRef {
 		if itemBoundToRef.OperationKind == OperationKindDelete {
-			delete(se.State.ItemBoundToRef, itemBoundToRef.ID)
+			delete(engine.State.ItemBoundToRef, itemBoundToRef.ID)
 		} else {
 			itemBoundToRef.OperationKind = OperationKindUnchanged
-			se.State.ItemBoundToRef[itemBoundToRef.ID] = itemBoundToRef
+			engine.State.ItemBoundToRef[itemBoundToRef.ID] = itemBoundToRef
 		}
 	}
-	for _, playerEquipmentSetRef := range se.Patch.PlayerEquipmentSetRef {
+	for _, playerEquipmentSetRef := range engine.Patch.PlayerEquipmentSetRef {
 		if playerEquipmentSetRef.OperationKind == OperationKindDelete {
-			delete(se.State.PlayerEquipmentSetRef, playerEquipmentSetRef.ID)
+			delete(engine.State.PlayerEquipmentSetRef, playerEquipmentSetRef.ID)
 		} else {
 			playerEquipmentSetRef.OperationKind = OperationKindUnchanged
-			se.State.PlayerEquipmentSetRef[playerEquipmentSetRef.ID] = playerEquipmentSetRef
+			engine.State.PlayerEquipmentSetRef[playerEquipmentSetRef.ID] = playerEquipmentSetRef
 		}
 	}
-	for _, playerGuildMemberRef := range se.Patch.PlayerGuildMemberRef {
+	for _, playerGuildMemberRef := range engine.Patch.PlayerGuildMemberRef {
 		if playerGuildMemberRef.OperationKind == OperationKindDelete {
-			delete(se.State.PlayerGuildMemberRef, playerGuildMemberRef.ID)
+			delete(engine.State.PlayerGuildMemberRef, playerGuildMemberRef.ID)
 		} else {
 			playerGuildMemberRef.OperationKind = OperationKindUnchanged
-			se.State.PlayerGuildMemberRef[playerGuildMemberRef.ID] = playerGuildMemberRef
+			engine.State.PlayerGuildMemberRef[playerGuildMemberRef.ID] = playerGuildMemberRef
 		}
 	}
-	for _, playerTargetRef := range se.Patch.PlayerTargetRef {
+	for _, playerTargetRef := range engine.Patch.PlayerTargetRef {
 		if playerTargetRef.OperationKind == OperationKindDelete {
-			delete(se.State.PlayerTargetRef, playerTargetRef.ID)
+			delete(engine.State.PlayerTargetRef, playerTargetRef.ID)
 		} else {
 			playerTargetRef.OperationKind = OperationKindUnchanged
-			se.State.PlayerTargetRef[playerTargetRef.ID] = playerTargetRef
+			engine.State.PlayerTargetRef[playerTargetRef.ID] = playerTargetRef
 		}
 	}
-	for _, playerTargetedByRef := range se.Patch.PlayerTargetedByRef {
+	for _, playerTargetedByRef := range engine.Patch.PlayerTargetedByRef {
 		if playerTargetedByRef.OperationKind == OperationKindDelete {
-			delete(se.State.PlayerTargetedByRef, playerTargetedByRef.ID)
+			delete(engine.State.PlayerTargetedByRef, playerTargetedByRef.ID)
 		} else {
 			playerTargetedByRef.OperationKind = OperationKindUnchanged
-			se.State.PlayerTargetedByRef[playerTargetedByRef.ID] = playerTargetedByRef
+			engine.State.PlayerTargetedByRef[playerTargetedByRef.ID] = playerTargetedByRef
 		}
 	}
-	for _, anyOfItemPlayerZoneItem := range se.Patch.AnyOfItemPlayerZoneItem {
-		if anyOfItemPlayerZoneItem.OperationKind == OperationKindDelete {
-			delete(se.State.AnyOfItemPlayerZoneItem, anyOfItemPlayerZoneItem.ID)
-		} else {
-			anyOfItemPlayerZoneItem.OperationKind = OperationKindUnchanged
-			se.State.AnyOfItemPlayerZoneItem[anyOfItemPlayerZoneItem.ID] = anyOfItemPlayerZoneItem
-		}
-	}
-	for _, anyOfPlayerPosition := range se.Patch.AnyOfPlayerPosition {
+	for _, anyOfPlayerPosition := range engine.Patch.AnyOfPlayerPosition {
 		if anyOfPlayerPosition.OperationKind == OperationKindDelete {
-			delete(se.State.AnyOfPlayerPosition, anyOfPlayerPosition.ID)
+			delete(engine.State.AnyOfPlayerPosition, anyOfPlayerPosition.ID)
 		} else {
 			anyOfPlayerPosition.OperationKind = OperationKindUnchanged
-			se.State.AnyOfPlayerPosition[anyOfPlayerPosition.ID] = anyOfPlayerPosition
+			engine.State.AnyOfPlayerPosition[anyOfPlayerPosition.ID] = anyOfPlayerPosition
 		}
 	}
-	for _, anyOfPlayerZoneItem := range se.Patch.AnyOfPlayerZoneItem {
+	for _, anyOfPlayerZoneItem := range engine.Patch.AnyOfPlayerZoneItem {
 		if anyOfPlayerZoneItem.OperationKind == OperationKindDelete {
-			delete(se.State.AnyOfPlayerZoneItem, anyOfPlayerZoneItem.ID)
+			delete(engine.State.AnyOfPlayerZoneItem, anyOfPlayerZoneItem.ID)
 		} else {
 			anyOfPlayerZoneItem.OperationKind = OperationKindUnchanged
-			se.State.AnyOfPlayerZoneItem[anyOfPlayerZoneItem.ID] = anyOfPlayerZoneItem
+			engine.State.AnyOfPlayerZoneItem[anyOfPlayerZoneItem.ID] = anyOfPlayerZoneItem
+		}
+	}
+	for _, anyOfItemPlayerZoneItem := range engine.Patch.AnyOfItemPlayerZoneItem {
+		if anyOfItemPlayerZoneItem.OperationKind == OperationKindDelete {
+			delete(engine.State.AnyOfItemPlayerZoneItem, anyOfItemPlayerZoneItem.ID)
+		} else {
+			anyOfItemPlayerZoneItem.OperationKind = OperationKindUnchanged
+			engine.State.AnyOfItemPlayerZoneItem[anyOfItemPlayerZoneItem.ID] = anyOfItemPlayerZoneItem
 		}
 	}
 
-	for key := range se.Patch.EquipmentSet {
-		delete(se.Patch.EquipmentSet, key)
+	for key := range engine.Patch.EquipmentSet {
+		delete(engine.Patch.EquipmentSet, key)
 	}
-	for key := range se.Patch.GearScore {
-		delete(se.Patch.GearScore, key)
+	for key := range engine.Patch.GearScore {
+		delete(engine.Patch.GearScore, key)
 	}
-	for key := range se.Patch.Item {
-		delete(se.Patch.Item, key)
+	for key := range engine.Patch.Item {
+		delete(engine.Patch.Item, key)
 	}
-	for key := range se.Patch.Player {
-		delete(se.Patch.Player, key)
+	for key := range engine.Patch.Player {
+		delete(engine.Patch.Player, key)
 	}
-	for key := range se.Patch.Position {
-		delete(se.Patch.Position, key)
+	for key := range engine.Patch.Position {
+		delete(engine.Patch.Position, key)
 	}
-	for key := range se.Patch.Zone {
-		delete(se.Patch.Zone, key)
+	for key := range engine.Patch.Zone {
+		delete(engine.Patch.Zone, key)
 	}
-	for key := range se.Patch.ZoneItem {
-		delete(se.Patch.ZoneItem, key)
+	for key := range engine.Patch.ZoneItem {
+		delete(engine.Patch.ZoneItem, key)
 	}
-	for key := range se.Patch.EquipmentSetEquipmentRef {
-		delete(se.Patch.EquipmentSetEquipmentRef, key)
+	for key := range engine.Patch.EquipmentSetEquipmentRef {
+		delete(engine.Patch.EquipmentSetEquipmentRef, key)
 	}
-	for key := range se.Patch.ItemBoundToRef {
-		delete(se.Patch.ItemBoundToRef, key)
+	for key := range engine.Patch.ItemBoundToRef {
+		delete(engine.Patch.ItemBoundToRef, key)
 	}
-	for key := range se.Patch.PlayerEquipmentSetRef {
-		delete(se.Patch.PlayerEquipmentSetRef, key)
+	for key := range engine.Patch.PlayerEquipmentSetRef {
+		delete(engine.Patch.PlayerEquipmentSetRef, key)
 	}
-	for key := range se.Patch.PlayerGuildMemberRef {
-		delete(se.Patch.PlayerGuildMemberRef, key)
+	for key := range engine.Patch.PlayerGuildMemberRef {
+		delete(engine.Patch.PlayerGuildMemberRef, key)
 	}
-	for key := range se.Patch.PlayerTargetRef {
-		delete(se.Patch.PlayerTargetRef, key)
+	for key := range engine.Patch.PlayerTargetRef {
+		delete(engine.Patch.PlayerTargetRef, key)
 	}
-	for key := range se.Patch.PlayerTargetedByRef {
-		delete(se.Patch.PlayerTargetedByRef, key)
+	for key := range engine.Patch.PlayerTargetedByRef {
+		delete(engine.Patch.PlayerTargetedByRef, key)
 	}
-	for key := range se.Patch.AnyOfItemPlayerZoneItem {
-		delete(se.Patch.AnyOfItemPlayerZoneItem, key)
+	for key := range engine.Patch.AnyOfPlayerPosition {
+		delete(engine.Patch.AnyOfPlayerPosition, key)
 	}
-	for key := range se.Patch.AnyOfPlayerPosition {
-		delete(se.Patch.AnyOfPlayerPosition, key)
+	for key := range engine.Patch.AnyOfPlayerZoneItem {
+		delete(engine.Patch.AnyOfPlayerZoneItem, key)
 	}
-	for key := range se.Patch.AnyOfPlayerZoneItem {
-		delete(se.Patch.AnyOfPlayerZoneItem, key)
+	for key := range engine.Patch.AnyOfItemPlayerZoneItem {
+		delete(engine.Patch.AnyOfItemPlayerZoneItem, key)
 	}
 }
--- enginefactory/write_state_engine.go
+++ enginefactory/write_state_engine.go
@@ -14,7 +14,8 @@ func (s *EngineFactory) writeOperationKind() *EngineFactory {
 
 	decls.File.Const().Defs(
 		Id("OperationKindDelete").Id("OperationKind").Op("=").Lit("DELETE"),
-		Id("OperationKindUpdate").Op("=").Lit("UPDATE"),
+		Id("OperationKindUpdate").Id("OperationKind").Op("=").Lit("UPDATE"),
+		Id("OperationKindUnchanged").Id("OperationKind").Op("=").Lit("UNCHANGED"),
 	)
 
 	decls.Render(s.buf)
@@ -27,14 +28,18 @@ func (s *EngineFactory) writeEngine() *EngineFactory {
 	decls.File.Type().Id("Engine").Struct(
 		Id("State").Id("State"),
 		Id("Patch").Id("State"),
+		Id("Tree").Id("Tree"),
+		Id("PathTrack").Id("pathTrack"),
 		Id("IDgen").Int(),
 	)
 
 	decls.File.Func().Id("newEngine").Params().Id("*Engine").Block(
 		Return(Id("&Engine").Values(Dict{
-			Id("State"): Id("newState").Call(),
-			Id("Patch"): Id("newState").Call(),
-			Id("IDgen"): Lit(1),
+			Id("State"):     Id("newState").Call(),
+			Id("Patch"):     Id("newState").Call(),
+			Id("Tree"):      Id("newTree").Call(),
+			Id("PathTrack"): Id("newPathTrack").Call(),
+			Id("IDgen"):     Lit(1),
 		})),
 	)
 
@@ -45,9 +50,9 @@ func (s *EngineFactory) writeEngine() *EngineFactory {
 func (s *EngineFactory) writeGenerateID() *EngineFactory {
 	decls := NewDeclSet()
 
-	decls.File.Func().Params(Id("se").Id("*Engine")).Id("GenerateID").Params().Int().Block(
-		Id("newID").Op(":=").Id("se").Dot("IDgen"),
-		Id("se").Dot("IDgen").Op("=").Id("se").Dot("IDgen").Op("+").Lit(1),
+	decls.File.Func().Params(Id("engine").Id("*Engine")).Id("GenerateID").Params().Int().Block(
+		Id("newID").Op(":=").Id("engine").Dot("IDgen"),
+		Id("engine").Dot("IDgen").Op("=").Id("engine").Dot("IDgen").Op("+").Lit(1),
 		Return(Id("newID")),
 	)
 
@@ -58,21 +63,47 @@ func (s *EngineFactory) writeGenerateID() *EngineFactory {
 func (s *EngineFactory) writeUpdateState() *EngineFactory {
 	decls := NewDeclSet()
 
-	u := updateState{}
+	u := updateStateWriter{}
 
 	decls.File.Func().Params(u.receiverParams()).Id("UpdateState").Params().Block(
 		ForEachTypeInAST(s.config, func(configType ast.ConfigType) *Statement {
-			u.t = &configType
-			return For(u.loopPatchElementsConditions()).Block(
-				If(u.isOperationKindDelete()).Block(
-					u.deleteElement(),
-				).Else().Block(
-					u.updateElement(),
-				),
-			)
+			u.typeName = func() string {
+				return configType.Name
+			}
+			return writeUpdateElement(u)
+		}),
+		ForEachRefFieldInAST(s.config, func(field ast.Field) *Statement {
+			u.typeName = func() string {
+				return field.ValueTypeName
+			}
+			return writeUpdateElement(u)
+		}),
+		ForEachAnyFieldInAST(s.config, func(field ast.Field) *Statement {
+			u.typeName = func() string {
+				return anyNameByField(field)
+			}
+			return writeUpdateElement(u)
 		}),
 		ForEachTypeInAST(s.config, func(configType ast.ConfigType) *Statement {
-			u.t = &configType
+			u.typeName = func() string {
+				return configType.Name
+			}
+			return For(u.loopPatchKeysConditions()).Block(
+				u.clearElementFromPatch(),
+			)
+		}),
+		ForEachRefFieldInAST(s.config, func(field ast.Field) *Statement {
+			u.typeName = func() string {
+				return field.ValueTypeName
+			}
+			return For(u.loopPatchKeysConditions()).Block(
+				u.clearElementFromPatch(),
+			)
+		}),
+		ForEachAnyFieldInAST(s.config, func(field ast.Field) *Statement {
+			u.typeName = func() string {
+				return anyNameByField(field)
+			}
 			return For(u.loopPatchKeysConditions()).Block(
 				u.clearElementFromPatch(),
 			)
@@ -83,34 +114,49 @@ func (s *EngineFactory) writeUpdateState() *EngineFactory {
 	return s
 }
 
-type updateState struct {
-	t *ast.ConfigType
+func writeUpdateElement(u updateStateWriter) *Statement {
+	return For(u.loopPatchElementsConditions()).Block(
+		If(u.isOperationKindDelete()).Block(
+			u.deleteElement(),
+		).Else().Block(
+			u.setOperationKindUnchanged(),
+			u.updateElement(),
+		),
+	)
 }
 
-func (u updateState) receiverParams() *Statement {
-	return Id("se").Id("*Engine")
+type updateStateWriter struct {
+	typeName func() string
 }
 
-func (u updateState) loopPatchElementsConditions() *Statement {
-	return List(Id("_"), Id(u.t.Name)).Op(":=").Range().Id("se").Dot("Patch").Dot(title(u.t.Name))
+func (u updateStateWriter) receiverParams() *Statement {
+	return Id("engine").Id("*Engine")
 }
 
-func (u updateState) isOperationKindDelete() *Statement {
-	return Id(u.t.Name).Dot("OperationKind_").Op("==").Id("OperationKindDelete")
+func (u updateStateWriter) loopPatchElementsConditions() *Statement {
+	return List(Id("_"), Id(u.typeName())).Op(":=").Range().Id("engine").Dot("Patch").Dot(title(u.typeName()))
 }
 
-func (u updateState) deleteElement() *Statement {
-	return Id("delete").Call(Id("se").Dot("State").Dot(title(u.t.Name)), Id(u.t.Name).Dot("ID"))
+func (u updateStateWriter) isOperationKindDelete() *Statement {
+	return Id(u.typeName()).Dot("OperationKind").Op("==").Id("OperationKindDelete")
 }
 
-func (u updateState) updateElement() *Statement {
-	return Id("se").Dot("State").Dot(title(u.t.Name)).Index(Id(u.t.Name).Dot("ID")).Op("=").Id(u.t.Name)
+func (u updateStateWriter) deleteElement() *Statement {
+	return Id("delete").Call(Id("engine").Dot("State").Dot(title(u.typeName())), Id(u.typeName()).Dot("ID"))
 }
 
-func (u updateState) loopPatchKeysConditions() *Statement {
-	return List(Id("key")).Op(":=").Range().Id("se").Dot("Patch").Dot(title(u.t.Name))
+func (u updateStateWriter) setOperationKindUnchanged() *Statement {
+	return Id(u.typeName()).Dot("OperationKind").Op("=").Id("OperationKindUnchanged")
 }
 
-func (u updateState) clearElementFromPatch() *Statement {
-	return Id("delete").Call(Id("se").Dot("Patch").Dot(title(u.t.Name)), Id("key"))
+func (u updateStateWriter) updateElement() *Statement {
+	return Id("engine").Dot("State").Dot(title(u.typeName())).Index(Id(u.typeName()).Dot("ID")).Op("=").Id(u.typeName())
+}
+
+func (u updateStateWriter) loopPatchKeysConditions() *Statement {
+	return List(Id("key")).Op(":=").Range().Id("engine").Dot("Patch").Dot(title(u.typeName()))
+}
+
+func (u updateStateWriter) clearElementFromPatch() *Statement {
+	return Id("delete").Call(Id("engine").Dot("Patch").Dot(title(u.typeName())), Id("key"))
 }
