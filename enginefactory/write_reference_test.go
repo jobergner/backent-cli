730d62285633105b77c8fa57ebec12d8be06282b
Jonas Bergner <jonas.bergner96@gmail.com>
2021-05-27 18:28:24 +0200

    write reference

--- enginefactory/write_reference_test.go
+++ enginefactory/write_reference_test.go
@@ -14,15 +14,23 @@ func TestWriteReference(t *testing.T) {
 		actual := testutils.FormatCode(sf.buf.String())
 		expected := testutils.FormatCode(strings.Join([]string{
 			_Get_equipmentSetEquipmentRef_func,
+			dereferenceEquipmentSetEquipmentRefs_Engine_func,
 			_IsSet_itemBoundToRef_func,
 			_Unset_itemBoundToRef_func,
 			_Get_itemBoundToRef_func,
+			dereferenceItemBoundToRefs_Engine_func,
 			_Get_playerEquipmentSetRef_func,
+			dereferencePlayerEquipmentSetRefs_Engine_func,
 			_Get_playerGuildMemberRef_func,
+			dereferencePlayerGuildMemberRefs_Engine_func,
 			_IsSet_playerTargetRef_func,
 			_Unset_playerTargetRef_func,
 			_Get_playerTargetRef_func,
+			dereferencePlayerTargetRefsPlayer_Engine_func,
+			dereferencePlayerTargetRefsZoneItem_Engine_func,
 			_Get_playerTargetedByRef_func,
+			dereferencePlayerTargetedByRefsPlayer_Engine_func,
+			dereferencePlayerTargetedByRefsZoneItem_Engine_func,
 		}, "\n"))
 
 		if expected != actual {
