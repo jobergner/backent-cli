a26eba6f8794941fdf8dad1d9e8713468311e59e
Jonas Bergner <jonas.bergner96@gmail.com>
2021-05-28 20:53:33 +0200

    write setters

--- enginefactory/write_setters.go
+++ enginefactory/write_setters.go
@@ -16,7 +16,7 @@ func (s *EngineFactory) writeSetters() *EngineFactory {
 				return
 			}
 
-			s := setter{
+			s := setterWriter{
 				t: configType,
 				f: field,
 			}
@@ -34,66 +34,40 @@ func (s *EngineFactory) writeSetters() *EngineFactory {
 		})
 	})
 
+	s.config.RangeRefFields(func(field ast.Field) {
+		if field.HasSliceValue {
+			return
+		}
+
+		s := setRefFieldWeiter{
+			f: field,
+		}
+
+		field.RangeValueTypes(func(valueType *ast.ConfigType) {
+			s.v = valueType
+			decls.File.Func().Params(s.receiverParams()).Id(s.name()).Params(s.params()).Id(s.returns()).Block(
+				s.reassignElement(),
+				If(s.isOperationKindDelete()).Block(
+					Return(Id(field.Parent.Name)),
+				),
+				If(s.isReferencedElementDeleted()).Block(
+					Return(Id(field.Parent.Name)),
+				),
+				If(s.isRefAlreadyAssigned()).Block(
+					s.deleteExistingRef(),
+				),
+				onlyIf(field.HasAnyValue, s.createAnyContainer()),
+				onlyIf(field.HasAnyValue, s.setAnyContainer()),
+				s.createNewRef(),
+				s.setNewRef(),
+				s.setOperationKind(),
+				s.setItemInPatch(),
+				Return(Id(field.Parent.Name)),
+			)
+		})
+
+	})
+
 	decls.Render(s.buf)
 	return s
-}
-
-type setter struct {
-	t ast.ConfigType
-	f ast.Field
-}
-
-func (s setter) receiverParams() *Statement {
-	return Id(s.receiverName()).Id(s.t.Name)
-}
-
-func (s setter) name() string {
-	if s.f.ValueType().IsBasicType {
-		return "Set" + title(s.f.Name)
-	}
-	return "Add" + title(pluralizeClient.Singular(s.f.Name))
-}
-
-func (s setter) newValueParam() string {
-	return "new" + title(s.f.Name)
-}
-
-func (s setter) params() *Statement {
-	return List(Id("se").Id("*Engine"), Id(s.newValueParam()).Id(s.f.ValueType().Name))
-}
-
-func (s setter) returns() string {
-	return s.t.Name
-}
-
-func (s setter) reassignElement() *Statement {
-	return Id(s.t.Name).Op(":=").Id("se").Dot(title(s.t.Name)).Call(Id(s.receiverName()).Dot(s.t.Name).Dot("ID"))
-}
-
-func (s setter) isOperationKindDelete() *Statement {
-	return Id(s.t.Name).Dot(s.t.Name).Dot("OperationKind_").Op("==").Id("OperationKindDelete")
-}
-
-func (s setter) setAttribute() *Statement {
-	return Id(s.t.Name).Dot(s.t.Name).Dot(title(s.f.Name)).Op("=").Id(s.newValueParam())
-}
-
-func (s setter) setOperationKind() *Statement {
-	return Id(s.t.Name).Dot(s.t.Name).Dot("OperationKind_").Op("=").Id("OperationKindUpdate")
-}
-
-func (s setter) setOperationKindUpdate() *Statement {
-	return Id(s.t.Name).Dot(s.t.Name).Dot("OperationKind_").Op("=").Id("OperationKindUpdate")
-}
-
-func (s setter) updateElementInPatch() *Statement {
-	return Id("se").Dot("Patch").Dot(title(s.t.Name)).Index(s.elementID()).Op("=").Id(s.t.Name).Dot(s.t.Name)
-}
-
-func (s setter) elementID() *Statement {
-	return Id(s.t.Name).Dot(s.t.Name).Dot("ID")
-}
-
-func (s setter) receiverName() string {
-	return "_" + s.t.Name
 }
--- enginefactory/write_setters_test.go
+++ enginefactory/write_setters_test.go
@@ -11,12 +11,17 @@ func TestWriteSetters(t *testing.T) {
 		sf := newStateFactory(newSimpleASTExample())
 		sf.writeSetters()
 
-		actual := sf.buf.String()
+		actual := testutils.FormatCode(sf.buf.String())
 		expected := testutils.FormatCode(strings.Join([]string{
+			_SetName_equipmentSet_func,
 			_SetLevel_gearScore_func,
 			_SetScore_gearScore_func,
+			_SetName_item_func,
 			_SetX_position_func,
 			_SetY_position_func,
+			_SetBoundTo_item_func,
+			_SetTargetPlayer_player_func,
+			_SetTargetZoneItem_player_func,
 		}, "\n"))
 
 		if expected != actual {
