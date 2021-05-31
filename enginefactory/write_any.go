d12b86a65514923ae123f048daa721e34490e099
Java-Jonas <jonas.bergner96@gmail.com>
2021-05-31 15:48:07 +0200

    clean up any writer

--- enginefactory/write_any.go
+++ enginefactory/write_any.go
@@ -11,40 +11,53 @@ func (s *EngineFactory) writeAny() *EngineFactory {
 	decls := NewDeclSet()
 	s.config.RangeAnyFields(func(field ast.Field) {
 
-		decls.File.Func().Params(Id("_any").Id(anyNameByField(field))).Id("Kind").Params().Id("ElementKind").Block(
-			Id("any").Op(":=").Id("_any").Dot(anyNameByField(field)).Dot("engine").Dot(anyNameByField(field)).Call(Id("_any").Dot(anyNameByField(field)).Dot("ID")),
-			Return(Id("any").Dot(anyNameByField(field)).Dot("ElementKind")),
+		k := anyKindWriter{
+			f: field,
+		}
+
+		decls.File.Func().Params(k.receiverParams()).Id("Kind").Params().Id("ElementKind").Block(
+			k.reassignAnyContainer(),
+			Return(k.containedElementKind()),
 		)
 
-		field.RangeValueTypes(func(configType *ast.ConfigType) {
-			decls.File.Func().Params(Id("_any").Id(anyNameByField(field))).Id("Set"+title(configType.Name)).Params().Id(configType.Name).Block(
-				Id(configType.Name).Op(":=").Id("_any").Dot(anyNameByField(field)).Dot("engine").Dot("create"+title(configType.Name)).Call(True()),
-				Id("_any").Dot(anyNameByField(field)).Dot("set"+title(configType.Name)).Call(Id(configType.Name).Dot("ID").Call()),
-				Return(Id(configType.Name)),
+		field.RangeValueTypes(func(valueType *ast.ConfigType) {
+			s := anySetterWriter{
+				f: field,
+				v: *valueType,
+			}
+			decls.File.Func().Params(s.wrapperReceiverParams()).Id("Set"+title(valueType.Name)).Params().Id(valueType.Name).Block(
+				s.createChild(),
+				s.callSetter(),
+				Return(Id(valueType.Name)),
 			)
-			decls.File.Func().Params(Id("_any").Id(anyNameByField(field)+"Core")).Id("set"+title(configType.Name)).Params(Id(configType.Name+"ID").Id(title(configType.Name+"ID"))).Block(
-				Id("any").Op(":=").Id("_any").Dot("engine").Dot(anyNameByField(field)).Call(Id("_any").Dot("ID")).Dot(anyNameByField(field)),
-				ForEachValueOfField(field, func(valueType *ast.ConfigType) *Statement {
-					if valueType.Name == configType.Name {
+			decls.File.Func().Params(s.receiverParams()).Id("set"+title(valueType.Name)).Params(s.params()).Block(
+				s.reassignAnyContainer(),
+				ForEachValueOfField(field, func(_valueType *ast.ConfigType) *Statement {
+					if _valueType.Name == valueType.Name {
 						return Empty()
 					}
-					return If(Id("any").Dot(title(valueType.Name)).Op("!=").Lit(0)).Block(
-						Id("any").Dot("engine").Dot("delete"+title(valueType.Name)).Call(Id("any").Dot(title(valueType.Name))),
-						Id("any").Dot(title(valueType.Name)).Op("=").Lit(0),
+					s._v = _valueType
+					return If(s.otherValueIsSet()).Block(
+						s.deleteOtherValue(),
+						s.unsetIDInContainer(),
 					)
 				}),
-				Id("any").Dot("ElementKind").Op("=").Id("ElementKind"+title(configType.Name)),
-				Id("any").Dot(title(configType.Name)).Op("=").Id(configType.Name+"ID"),
-				Id("any").Dot("engine").Dot("Patch").Dot(title(anyNameByField(field))).Index(Id("any").Dot("ID")).Op("=").Id("any"),
+				s.setElementKind(),
+				s.setChildID(),
+				s.updateContainerInPatch(),
 			)
 		})
 
-		decls.File.Func().Params(Id("_any").Id(anyNameByField(field)+"Core")).Id("deleteChild").Params().Block(
-			Id("any").Op(":=").Id("_any").Dot("engine").Dot(anyNameByField(field)).Call(Id("_any").Dot("ID")).Dot(anyNameByField(field)),
+		d := anyDeleteChildWriter{
+			f: field,
+		}
+		decls.File.Func().Params(d.receiverParams()).Id("deleteChild").Params().Block(
+			d.reassignAnyContainer(),
 			Switch(Id("any").Dot("ElementKind")).Block(
 				ForEachValueOfField(field, func(valueType *ast.ConfigType) *Statement {
+					d.v = valueType
 					return Case(Id("ElementKind" + title(valueType.Name))).Block(
-						Id("any").Dot("engine").Dot("delete" + title(valueType.Name)).Call(Id("any").Dot(title(valueType.Name))),
+						d.deleteChild(),
 					)
 				}),
 			),
