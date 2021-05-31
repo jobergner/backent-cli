2a0ef4c98f89d3f298c1b7e7eb4c3d0d92ad4178
Java-Jonas <jonas.bergner96@gmail.com>
2021-05-31 14:58:25 +0200

    clean up reference writer

--- enginefactory/write_reference_test.go
+++ enginefactory/write_reference_test.go
@@ -14,21 +14,33 @@ func TestWriteReference(t *testing.T) {
 		actual := testutils.FormatCode(sf.buf.String())
 		expected := testutils.FormatCode(strings.Join([]string{
 			_Get_equipmentSetEquipmentRef_func,
-			dereferenceEquipmentSetEquipmentRefs_Engine_func,
 			_IsSet_itemBoundToRef_func,
 			_Unset_itemBoundToRef_func,
 			_Get_itemBoundToRef_func,
-			dereferenceItemBoundToRefs_Engine_func,
 			_Get_playerEquipmentSetRef_func,
-			dereferencePlayerEquipmentSetRefs_Engine_func,
 			_Get_playerGuildMemberRef_func,
-			dereferencePlayerGuildMemberRefs_Engine_func,
 			_IsSet_playerTargetRef_func,
 			_Unset_playerTargetRef_func,
 			_Get_playerTargetRef_func,
+			_Get_playerTargetedByRef_func,
+		}, "\n"))
+
+		if expected != actual {
+			t.Errorf(testutils.Diff(actual, expected))
+		}
+	})
+	t.Run("writes dereference", func(t *testing.T) {
+		sf := newStateFactory(newSimpleASTExample())
+		sf.writeDereference()
+
+		actual := testutils.FormatCode(sf.buf.String())
+		expected := testutils.FormatCode(strings.Join([]string{
+			dereferenceEquipmentSetEquipmentRefs_Engine_func,
+			dereferenceItemBoundToRefs_Engine_func,
+			dereferencePlayerEquipmentSetRefs_Engine_func,
+			dereferencePlayerGuildMemberRefs_Engine_func,
 			dereferencePlayerTargetRefsPlayer_Engine_func,
 			dereferencePlayerTargetRefsZoneItem_Engine_func,
-			_Get_playerTargetedByRef_func,
 			dereferencePlayerTargetedByRefsPlayer_Engine_func,
 			dereferencePlayerTargetedByRefsZoneItem_Engine_func,
 		}, "\n"))
