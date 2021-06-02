4ce45b8b5b9aff1cf2ba8a1dd5d623ea6bc0dac2
Jonas Bergner <jonas.bergner96@gmail.com>
2021-06-02 17:45:55 +0200

    wip assemble writer

---
+++ enginefactory/state_engine_example/ass.go
@@ -0,0 +1,43 @@
+package state
+
+/*
+
+// -
+if treePosition, include, childHasUpdated := engine.assemblePosition(playerData.Position, check, config); include {
+
+// []
+if treeItem, include, childHasUpdated := engine.assembleItem(itemID, check, config); include {
+
+// []*
+engine.assemblePlayerGuildMemberRef(playerGuildMemberRefID, check, config); include {
+
+// *a
+if treePlayerTargetRef, include, childHasUpdated := engine.assemblePlayerTargetRef(playerID, check, config); include {
+
+// interactables []a
+if treeZoneItem, include, childHasUpdated := engine.assembleZoneItem(zoneItemID, check, config); include {
+
+	// targeted []*a
+ engine.assemblePlayerTargetedByRef(playerTargetedByRefID, check, config); include {
+
+// origin *a
+if treePosition, include, childHasUpdated := engine.assemblePosition(positionID, check, config); include {
+
+// boundTo *
+if treeItemBoundToRef, include, childHasUpdated := engine.assembleItemBoundToRef(itemID, check, config); include {
+
+	if !pointer && !any && !slice
+		return data.FieldName
+	if pointer && !any && !slice
+		configType.name
+	if pointer && slice
+		valueTypeName
+	else
+		value.name
+
+		//origin
+	anyOfPlayerPositionContainer := engine.anyOfPlayerPosition(itemData.Origin).anyOfPlayerPosition
+	// interactables
+		anyOfItemPlayerZoneItemContainer := engine.anyOfItemPlayerZoneItem(anyOfItemPlayerZoneItemID).anyOfItemPlayerZoneItem
+
+*/
