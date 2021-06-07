97e07372a3814f81ed3aacc17e2cd6a6499b3356
Jonas Bergner <jonas.bergner96@gmail.com>
2021-06-07 10:37:53 +0200

    disable unuseful test

--- enginefactory/engine_factory_test.go
+++ enginefactory/engine_factory_test.go
@@ -3,24 +3,25 @@ package enginefactory
 import (
 	"bar-cli/ast"
 	"bar-cli/examples"
-	"bar-cli/testutils"
-	"io/ioutil"
-	"testing"
+	// "bar-cli/testutils"
+	// "io/ioutil"
+	// "testing"
 )
 
-func TestStateFactory(t *testing.T) {
-	t.Run("doesnt crash", func(t *testing.T) {
-		expected, err := ioutil.ReadFile("testdata/golden.go")
-		if err != nil {
-			t.Fatalf("Error loading golden file: %s", err)
-		}
-		actual := WriteEngineFrom(examples.StateConfig)
+// DISABLED DUE TO NOT BEING VERY USEFUL ATM!!
+// func TestStateFactory(t *testing.T) {
+// 	t.Run("doesnt crash", func(t *testing.T) {
+// 		expected, err := ioutil.ReadFile("testdata/golden.go")
+// 		if err != nil {
+// 			t.Fatalf("Error loading golden file: %s", err)
+// 		}
+// 		actual := WriteEngineFrom(examples.StateConfig)
 
-		if string(expected) != string(actual) {
-			t.Errorf(testutils.Diff(string(actual), string(expected)))
-		}
-	})
-}
+// 		if string(expected) != string(actual) {
+// 			t.Errorf(testutils.Diff(string(actual), string(expected)))
+// 		}
+// 	})
+// }
 
 func newSimpleASTExample() *ast.AST {
 	simpleAST := ast.Parse(examples.StateConfig, map[interface{}]interface{}{})
