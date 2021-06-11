45d7bd5476cc3982cdb75ad07ed2471f3c2b218c
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-11 15:01:56 +0200

    wip marshaller generating

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"bar-cli/serverfactory"
 	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
 )
+
+const outDir = "./tmp"
+const outFile = "state.go"
 
 func main() {
 	buf := bytes.NewBufferString("package state\n")
@@ -14,5 +20,13 @@ func main() {
 	writeImportedFiles(buf)
 	enginefactory.WriteEngine(buf, examples.StateConfig)
 	serverfactory.WriteServer(buf, examples.StateConfig, examples.ActionsConfig)
-	fmt.Println(buf.String())
+	if err := ioutil.WriteFile(filepath.Join(outDir, outFile), buf.Bytes(), 0644); err != nil {
+		panic(err)
+	}
+	cmd := exec.Command("easyjson", "-all", "-omit_empty", filepath.Join(outDir, outFile))
+	if out, err := cmd.Output(); err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(out))
+	}
 }
