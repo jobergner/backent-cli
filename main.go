dcf3a3a79f0e3f0efeca48b0e5868a5d41ba6749
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-11 17:11:49 +0200

    wip validating input configst

--- main.go
+++ main.go
@@ -6,21 +6,54 @@ import (
 	"bar-cli/getstartedfactory"
 	"bar-cli/serverfactory"
 	"bytes"
+	"flag"
 	"fmt"
+	"github.com/Java-Jonas/validator"
 	"io/ioutil"
 	"os/exec"
 	"path/filepath"
+
+	"gopkg.in/yaml.v2"
 )
 
 const outDir = "./tmp"
 const outFile = "state.go"
 
+type config struct {
+	State   map[interface{}]interface{} `yaml:"state"`
+	Actions map[interface{}]interface{} `yaml:"actions"`
+}
+
+var configNameFlag = flag.String("config", "./barcli.config.yaml", "path of config")
+
 func main() {
+	configFile, err := ioutil.ReadFile(*configNameFlag)
+	if err != nil {
+		panic(err)
+	}
+	c := config{}
+	err = yaml.Unmarshal([]byte(configFile), &c)
+	if err != nil {
+		panic(err)
+	}
+	if errs := validator.ValidateStateConfig(c.State); len(errs) != 0 {
+		for _, err := range errs {
+			fmt.Println(err)
+		}
+		panic("errors")
+	}
+	if errs := validator.ValidateActionsConfig(c.State, c.Actions); len(errs) != 0 {
+		for _, err := range errs {
+			fmt.Println(err)
+		}
+		panic("errors")
+	}
+	fmt.Printf("%+v\n", c)
 	buf := bytes.NewBufferString("package state\n")
 	writeCombinedImport(buf)
 	writeImportedFiles(buf)
-	enginefactory.WriteEngine(buf, examples.StateConfig)
-	serverfactory.WriteServer(buf, examples.StateConfig, examples.ActionsConfig)
+	enginefactory.WriteEngine(buf, c.State)
+	serverfactory.WriteServer(buf, c.State, c.Actions)
 	if err := ioutil.WriteFile(filepath.Join(outDir, outFile), buf.Bytes(), 0644); err != nil {
 		panic(err)
 	}
@@ -30,6 +63,5 @@ func main() {
 	} else {
 		fmt.Println(string(out))
 	}
-
 	fmt.Println(getstartedfactory.WriteGetStarted(examples.StateConfig, examples.ActionsConfig))
 }
