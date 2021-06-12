1f4cbddd9d268d033c0ea2475635e6fe5a6d9af6
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-12 16:56:09 +0200

    generator script for all generated code

--- enginefactory/state_engine_example/generate.sh
+++
@@ -1,1 +0,0 @@
-decltostring -input ./ -output ../stringified_state_engine_decls.go -package enginefactory -exclude "test|easyjson" 
--- serverfactory/server_example/generate.sh
+++
@@ -1,1 +0,0 @@
-decltostring -input ./server/ -output ../stringified_server_decls.go -package serverfactory -only "gets_generated.go" 
