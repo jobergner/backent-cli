3493532eae0543fb7ad00bc637e519c97f86b6db
jonas <jonas.bergner96@googlemail.com>
2021-06-13 16:26:29 +0200

    improve error handling

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/Java-Jonas/bar-cli/getstartedfactory"
@@ -27,36 +28,46 @@ func main() {
 
 	validationErrs := validateConfig(c)
 	if len(validationErrs) != 0 {
-
 		for _, validationErr := range validationErrs {
 			fmt.Println(validationErr)
 		}
-
-		panic("errors during config validation")
+		panic("\nthe above errors have occured while validating " + *configNameFlag)
 	}
 
-	if err := ensureDir(); err != nil {
-		panic("err")
-	}
-
-	code := writeCode(c)
-	if err := ioutil.WriteFile(filepath.Join(*outDirname, outFile), code, 0644); err != nil {
+	if err := validateOutDir(); err != nil {
 		panic(err)
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(*outDirname, outFile), writeCode(c), 0644); err != nil {
+		panic(fmt.Errorf("error while writing generated code to file system: %s", err))
 	}
 
 	if err := generateMarshallers(); err != nil {
-		panic(err)
+		panic(fmt.Errorf("error while generating marshallers: %s", err))
 	}
 
 	fmt.Println(getstartedfactory.WriteGetStarted(c.State, c.Actions))
 }
 
-func ensureDir() error {
-	err := os.MkdirAll(*outDirname, os.ModeDir)
+func validateOutDir() error {
+	fi, err := os.Stat(*outDirname)
 
-	if err == nil || os.IsExist(err) {
-		return nil
-	} else {
-		return err
+	if err != nil {
+		return fmt.Errorf("defined out target \"%s\" does not exist", *outDirname)
 	}
+
+	mode := fi.Mode()
+	if !mode.IsDir() {
+		return fmt.Errorf("defined out target \"%s\" is not a directory", *outDirname)
+	}
+
+	cmd := exec.Command("go", "env", "GOMOD")
+	cmd.Dir = *outDirname
+
+	stdout, err := cmd.Output()
+	if len(stdout) == 1 && string(stdout[0]) == "\n" {
+		return fmt.Errorf("defined out target \"%s\" is not within GOPATH which is required for generating marshallers\ntip: initialize a go module in directory or its parent!", *outDirname)
+	}
+
+	return nil
 }
