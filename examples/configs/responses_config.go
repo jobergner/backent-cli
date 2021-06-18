d63ec30e7ecec944383ced1673aae68f5f963433
Jonas Bergner <jonas.bergner96@gmail.com>
2021-06-18 12:43:01 +0200

    server example with responses

---
+++ examples/configs/responses_config.go
@@ -0,0 +1,10 @@
+package configs
+
+var ResponsesConfig = map[interface{}]interface{}{
+	"addItemToPlayer": map[interface{}]interface{}{
+		"playerPath": "string",
+	},
+	"spawnZoneItems": map[interface{}]interface{}{
+		"newZoneItemPaths": "[]string",
+	},
+}
