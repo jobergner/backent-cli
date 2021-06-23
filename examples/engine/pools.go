3746757e872c2b36f44e1562b464dd2d1d3750cb
Jonas Bergner <jonas.bergner96@gmail.com>
2021-06-23 11:00:18 +0200

    fix initial slice length in pools

--- examples/engine/pools.go
+++ examples/engine/pools.go
@@ -8,89 +8,89 @@ var zoneItemCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[ZoneItemID]bool) },
 }
 var zoneItemIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]ZoneItemID, 10) },
+	New: func() interface{} { return make([]ZoneItemID, 0) },
 }
 
 var zoneCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[ZoneID]bool) },
 }
 var zoneIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]ZoneID, 10) },
+	New: func() interface{} { return make([]ZoneID, 0) },
 }
 
 var playerCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PlayerID]bool) },
 }
 var playerIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PlayerID, 10) },
+	New: func() interface{} { return make([]PlayerID, 0) },
 }
 
 var positionCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PositionID]bool) },
 }
 var positionIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PositionID, 10) },
+	New: func() interface{} { return make([]PositionID, 0) },
 }
 
 var itemCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[ItemID]bool) },
 }
 var itemIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]ItemID, 10) },
+	New: func() interface{} { return make([]ItemID, 0) },
 }
 
 var gearScoreCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[GearScoreID]bool) },
 }
 var gearScoreIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]GearScoreID, 10) },
+	New: func() interface{} { return make([]GearScoreID, 0) },
 }
 
 var equipmentSetCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[EquipmentSetID]bool) },
 }
 var equipmentSetIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]EquipmentSetID, 10) },
+	New: func() interface{} { return make([]EquipmentSetID, 0) },
 }
 
 var playerTargetedByRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PlayerTargetedByRefID]bool) },
 }
 var playerTargetedByRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PlayerTargetedByRefID, 10) },
+	New: func() interface{} { return make([]PlayerTargetedByRefID, 0) },
 }
 
 var playerTargetRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PlayerTargetRefID]bool) },
 }
 var playerTargetRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PlayerTargetRefID, 10) },
+	New: func() interface{} { return make([]PlayerTargetRefID, 0) },
 }
 
 var itemBoundToRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[ItemBoundToRefID]bool) },
 }
 var itemBoundToRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]ItemBoundToRefID, 10) },
+	New: func() interface{} { return make([]ItemBoundToRefID, 0) },
 }
 
 var playerGuildMemberRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PlayerGuildMemberRefID]bool) },
 }
 var playerGuildMemberRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PlayerGuildMemberRefID, 10) },
+	New: func() interface{} { return make([]PlayerGuildMemberRefID, 0) },
 }
 
 var playerEquipmentSetRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[PlayerEquipmentSetRefID]bool) },
 }
 var playerEquipmentSetRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]PlayerEquipmentSetRefID, 10) },
+	New: func() interface{} { return make([]PlayerEquipmentSetRefID, 0) },
 }
 
 var equipmentSetEquipmentRefCheckPool = sync.Pool{
 	New: func() interface{} { return make(map[EquipmentSetEquipmentRefID]bool) },
 }
 var equipmentSetEquipmentRefIDSlicePool = sync.Pool{
-	New: func() interface{} { return make([]EquipmentSetEquipmentRefID, 10) },
+	New: func() interface{} { return make([]EquipmentSetEquipmentRefID, 0) },
 }
