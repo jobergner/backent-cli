b8d4d108055a374908e9ad4a7ee4539d9238a7c2
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-27 19:03:54 +0200

    swallow unnecessary easyjson errors

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 # required for running main (and examples, as main is used to generate code for examples)
 go run generate/*;
 
-# required for running examples
+# required for running examples. initially generatin marshallers causes errors which we swallow
+easyjson -all -output_filename examples/application/server/gets_generated_easyjson.go examples/application/server/gets_generated.go &> /dev/null;
+easyjson -all -output_filename examples/application/server/message_easyjson.go examples/application/server/message.go &> /dev/null;
 go run . -engine_only -out examples/application/server/;
 easyjson -all -output_filename examples/engine/tree_easyjson.go examples/engine/tree.go;
-easyjson -all -output_filename examples/application/server/gets_generated_easyjson.go examples/application/server/gets_generated.go;
-easyjson -all -output_filename examples/application/server/message_easyjson.go examples/application/server/message.go;
 
 # required for running unit tests
 decltostring -input ./examples/application/server/ -output ./serverfactory/stringified_server_decls.go -package serverfactory -only "gets_generated.go";
