dd943858e1925e00762c1b357bbd538dd43938ba
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-29 10:41:33 +0200

    more drafting

--- src/Actions.js
+++ src/Actions.js
@@ -1,17 +1,19 @@
 import "./Actions.css";
 import Input from "./Input"
-import { Button, Card, H5, Classes, Intent } from "@blueprintjs/core";
+import { Icon, Button, Card, H5, Classes, Intent, Divider } from "@blueprintjs/core";
 import config from "./example.config.json"
 
 function Actions() {
-
     return (
         <>
-            {Object.entries(config.actions).map(([key, value], i) => {
-                console.log(key, value)
-                return <Card elevation={0} className="card" key={key}>
-                    <H5>Action: {key}</H5>
-                    <Input action={value} />
+            {Object.entries(config.actions).map(([key, value]) => {
+                return <Card elevation={0} className="card Action" key={key}>
+                    <H5><Icon className="HeadlineIcon" iconSize={17} icon="send-to" intent={Intent.PRIMARY} />{key}</H5>
+                    <Divider />
+                    <div className="InputsWrapper">
+                        <Input action={value} />
+                    </div>
+                    <Divider />
                     <Button intent={Intent.PRIMARY} text="Send" className={Classes.BUTTON} />
                 </Card>
             })}
