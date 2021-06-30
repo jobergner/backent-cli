6f68b4c47396b52aaf55b52292a1343b9341bc99
Java-Jonas <jonas.bergner96@gmail.com>
2021-06-30 14:27:55 +0200

    non ideal state for received content

--- src/ResponseCard.js
+++ src/ResponseCard.js
@@ -1,6 +1,7 @@
 import {Divider, Icon, Intent, Button, Card, H5, Classes } from "@blueprintjs/core";
 import ReactJson from 'react-json-view'
 import config from "./example.config.json"
+import Empty from "./Empty"
 
 
 function ResponseCard() {
@@ -9,7 +10,8 @@ function ResponseCard() {
         <H5><Icon className="HeadlineIcon" iconSize={17} icon="download" intent={Intent.PRIMARY} />Latest Response</H5>
         <Divider />
       <div className="JsonWrapper">
-        <ReactJson collapsed src={config} />
+        {/* <ReactJson collapsed src={config} /> */}
+        <Empty description={"Send an Action to receive a Response."}/>
       </div>
       </Card>
   );
