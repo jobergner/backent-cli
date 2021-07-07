0a1640c8747d02e68ed8fd9fe7b739eb2675c3ce
Java-Jonas <jonas.bergner96@gmail.com>
2021-07-07 17:24:45 +0200

    config display

--- src/InitialStateCard.js
+++ src/InitialStateCard.js
@@ -1,18 +1,31 @@
-import {Divider, Icon, Intent,  Card, H5 } from "@blueprintjs/core";
-import ReactJson from 'react-json-view'
-import Empty from "./Empty"
+import { Divider, Icon, Intent, Card, H5 } from "@blueprintjs/core";
+import ReactJson from "react-json-view";
+import Empty from "./Empty";
 
-
-function InitialStateCard({data}) {
+function InitialStateCard({ data }) {
   return (
-      <Card elevation={0} className="card card1">
-        <H5><Icon className="HeadlineIcon" iconSize={17} icon="diagram-tree" intent={Intent.PRIMARY} />Initial State</H5>
-        <Divider />
+    <Card elevation={0} className="card card1">
+      <H5>
+        <Icon
+          className="HeadlineIcon"
+          iconSize={17}
+          icon="diagram-tree"
+          intent={Intent.PRIMARY}
+        />
+        Initial State
+      </H5>
+      <Divider />
       <div className="JsonWrapper">
-      {data && <ReactJson collapsed src={data} />}
-      {!data && <Empty description={"Send an Action to receive a Response."}/>} 
+        {data && <ReactJson collapsed src={data} />}
+        {!data && (
+          <Empty
+            description={
+              "As soon as you connect to the server the current server state will appear here!"
+            }
+          />
+        )}
       </div>
-      </Card>
+    </Card>
   );
 }
 
