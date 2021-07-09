2b85802c84f31f13d88f5ae035b6de850b964d64
Jonas Bergner <jonas.bergner96@gmail.com>
2021-07-09 20:45:30 +0200

    implement commands

---
+++ inspect.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func inspect() {
+	fs := http.FileServer(http.Dir(staticServePath()))
+	http.Handle("/", fs)
+
+	fmt.Println("inspector running...")
+	fmt.Println("open your browser on http://localhost:3100")
+	fmt.Println("use url query parameter if the server you want to expect ist not listening on 3494")
+	fmt.Println("eg. http://localhost:3100?port=8080")
+
+	err := http.ListenAndServe(":3100", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func staticServePath() string {
+	if *devModeFlag {
+		return "inspector/build"
+	}
+	return gopath() + "/src/github.com/Java-Jonas/bar-cli/inspector/build"
+}
+
+func gopath() string {
+	p := os.Getenv("GOPATH")
+
+	if len(p) == 0 {
+		panic("could not find $GOPATH")
+	}
+
+	return p
+}
