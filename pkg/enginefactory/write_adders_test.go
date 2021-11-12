57e5d71755f300cceb3699f94b5e53c40a8d8f70
Jonas Bergner <jonas@fireballlabs.com>
2021-11-12 21:22:00 +0100

    fix: test variable names

--- pkg/enginefactory/write_adders_test.go
+++ pkg/enginefactory/write_adders_test.go
@@ -14,18 +14,18 @@ func TestWriteAdders(t *testing.T) {
 
 		actual := testutils.FormatCode(sf.buf.String())
 		expected := testutils.FormatCode(strings.Join([]string{
-			_AddEquipment_equipmentSet_func,
-			_AddEquipmentSet_player_func,
-			_AddGuildMember_player_func,
-			_AddItem_player_func,
-			_AddTargetedByPlayer_player_func,
-			_AddTargetedByZoneItem_player_func,
-			_AddInteractableItem_zone_func,
-			_AddInteractablePlayer_zone_func,
-			_AddInteractableZoneItem_zone_func,
-			_AddItem_zone_func,
-			_AddPlayer_zone_func,
-			_AddTags_zone_func,
+			_AddEquipment_EquipmentSet_func,
+			_AddEquipmentSet_Player_func,
+			_AddGuildMember_Player_func,
+			_AddItem_Player_func,
+			_AddTargetedByPlayer_Player_func,
+			_AddTargetedByZoneItem_Player_func,
+			_AddInteractableItem_Zone_func,
+			_AddInteractablePlayer_Zone_func,
+			_AddInteractableZoneItem_Zone_func,
+			_AddItem_Zone_func,
+			_AddPlayer_Zone_func,
+			_AddTags_Zone_func,
 		}, "\n"))
 
 		if expected != actual {
