8ff6c830cb02953cb348acfb116d13bff49fa77a
Jonas Bergner <jonas.bergner96@gmail.com>
2022-03-06 09:03:38 +0100

    fix: removers

--- pkg/factory/engine/write_removers.go
+++ pkg/factory/engine/write_removers.go
@@ -29,33 +29,16 @@ func (s *EngineFactory) writeRemovers() *EngineFactory {
 					If(r.isOperationKindDelete()).Block(
 						Return(Id(configType.Name)),
 					),
-					OnlyIf(r.f.HasAnyValue && r.f.HasPointerValue, &Statement{
-						Id("refs").Op(":=").Make(Map(Id(Title(r.f.ValueTypeName + "ID"))).Id(Title(anyNameByField(r.f) + "ID"))).Line(),
-						For(List(Id("_"), Id("refID")).Op(":=").Range().Add(r.elementCore()).Dot(Title(r.f.Name))).Block(
-							Id("ref").Op(":=").Add(r.engine()).Dot(r.f.ValueTypeName).Call(Id("refID")),
-							Id("refs").Index(Id("refID")).Op("=").Id("ref").Dot(r.f.ValueTypeName).Dot("ReferencedElementID"),
-						).Line(),
-					}),
-					OnlyIf(r.f.HasPointerValue || r.f.HasAnyValue, &Statement{
-						Id("wrappers").Op(":=").Make(Map(Id(Title(r.f.ValueTypeName + "ID"))).Id(Title(r.v.Name + "ID"))).Line(),
-						For(r.eachWrapper()).Block(
-							Id("wrapper").Op(":=").Add(r.getWrapper()),
-							OnlyIf(r.f.HasAnyValue, If(Id("wrapper").Dot("Kind").Call()).Op("!=").Id("ElementKind"+Title(r.v.Name)).Block(
-								Continue(),
-							)),
-							Id("wrappers").Index(r.usedWrapperID()).Op("=").Add(r.getElementID()),
-						).Line(),
-					}),
 					For(r.eachElement()).Block(
-						OnlyIf(r.f.HasAnyValue || r.f.HasPointerValue, Id(r.v.Name+"ID").Op(":=").Id("wrappers").Index(Id("wrapperID"))),
 						If(r.toRemoveComparator().Op("!=").Id(r.toRemoveParamName())).Block(
 							Continue(),
 						),
 						r.field().Index(Id("i")).Op("=").Add(r.field()).Index(Len(r.field()).Lit(-1)),
-						r.field().Index(Len(r.field()).Lit(-1)).Op("=").Lit(defaultValueForBasicType(r.v.Name)),
+						r.field().Index(Len(r.field()).Lit(-1)).Op("=").Add(r.zeroValueID()),
 						r.field().Op("=").Add(r.field()).Index(Id(""), Len(r.field()).Lit(-1)),
 						OnlyIf(!r.v.IsBasicType, r.deleteElement()),
 						r.elementCore().Dot("OperationKind").Op("=").Id("OperationKindUpdate"),
+						r.elementCore().Dot("Meta").Dot("sign").Call(r.engine().Dot("broadcastingClientID")),
 						r.engine().Dot("Patch").Dot(Title(r.t.Name)).Index(r.elementCore().Dot("ID")).Op("=").Add(r.elementCore()),
 						Break(),
 					),
