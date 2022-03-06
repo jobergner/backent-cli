fd1ad7536560181e44ea5fdab436a2de0ba08064
Jonas Bergner <jonas.bergner96@googlemail.com>
2022-03-06 09:03:38 +0100

    fix: removers

--- pkg/factory/engine/write_removers_writer.go
+++ pkg/factory/engine/write_removers_writer.go
@@ -71,59 +71,21 @@ func (r remover) field() *Statement {
 	return r.elementCore().Dot(Title(r.f.Name))
 }
 
-func (r remover) eachWrapperRangee() *Statement {
+func (r remover) zeroValueID() *Statement {
 	switch {
-	case !r.f.HasPointerValue || !r.f.HasAnyValue:
-		return r.field()
+	case r.f.HasAnyValue || r.f.HasPointerValue:
+		return Id(Title(r.f.ValueTypeName) + "ID").Values()
 	default:
-		return Id("refs")
-	}
-}
-
-func (r remover) getWrapper() *Statement {
-	switch {
-	case r.f.HasAnyValue:
-		return r.engine().Dot(anyNameByField(r.f)).Call(Id("wrapperID"))
-	default:
-		return r.engine().Dot(r.f.ValueTypeName).Call(Id("wrapperID"))
-	}
-}
-
-func (r remover) eachWrapperIndexLit() *Statement {
-	switch {
-	case !r.f.HasPointerValue || !r.f.HasAnyValue:
-		return Id("_")
-	default:
-		return Id("refID")
-	}
-}
-
-func (r remover) eachWrapper() *Statement {
-	return List(r.eachWrapperIndexLit(), Id("wrapperID")).Op(":=").Range().Add(r.eachWrapperRangee())
-}
-
-func (r remover) usedWrapperID() *Statement {
-	switch {
-	case r.f.HasPointerValue && r.f.HasAnyValue:
-		return Id("refID")
-	default:
-		return Id("wrapperID")
-	}
-}
-
-func (r remover) getElementID() *Statement {
-	switch {
-	case r.f.HasAnyValue:
-		return Id("wrapper").Dot(Title(r.v.Name)).Call().Dot("ID").Call()
-	default:
-		return Id("wrapper").Dot(r.f.ValueTypeName).Dot("ReferencedElementID")
+		return Lit(0)
 	}
 }
 
 func (r remover) toRemoveComparator() *Statement {
 	switch {
 	case r.v.IsBasicType:
-		return Id("val")
+		return r.engine().Dot(BasicTypes[r.v.Name]).Call(Id("valID")).Dot("Value")
+	case r.f.HasAnyValue || r.f.HasPointerValue:
+		return Id(Title(r.v.Name) + "ID").Call(Id("complexID").Dot("ChildID"))
 	default:
 		return Id(r.v.Name + "ID")
 	}
@@ -132,11 +94,11 @@ func (r remover) toRemoveComparator() *Statement {
 func (r remover) eachElementLiteral() *Statement {
 	switch {
 	case r.v.IsBasicType:
-		return Id("val")
+		return Id("valID")
 	case !r.f.HasAnyValue && !r.f.HasPointerValue:
 		return Id(r.v.Name + "ID")
 	default:
-		return Id("wrapperID")
+		return Id("complexID")
 	}
 }
 
