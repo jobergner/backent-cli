8338ffbc034796cbf932b98f9435d78f08bb9e47
Jonas Bergner <jonas.bergner96@gmail.com>
2022-03-07 18:56:30 +0100

    fix: assemble tree

--- pkg/factory/engine/write_assemble_tree.go
+++ pkg/factory/engine/write_assemble_tree.go
@@ -25,15 +25,15 @@ func (s *EngineFactory) writeAssembleTree() *EngineFactory {
 
 	s.file.Func().Params(Id("engine").Id("*Engine")).Id("assembleTree").Params().Block(
 		For(List(Id("_"), Id("p")).Op(":=").Range().Id("engine").Dot("planner").Dot("updatedPaths")).Block(
-			Switch(Id("p").Index(Lit(0)).Dot("identifier")).Block(
+			Switch(Id("p").Index(Lit(0)).Dot("Identifier")).Block(
 				ForEachTypeInAST(s.config, func(configType ast.ConfigType) *Statement {
 					return Case(Id(configType.Name+"Identifier")).Block(
-						List(Id("child"), Id("ok")).Op(":=").Id("engine").Dot("Tree").Dot(Title(configType.Name)).Index(Id(Title(configType.Name)+"ID").Call(Id("p").Index(Lit(0)).Dot("id"))),
+						List(Id("child"), Id("ok")).Op(":=").Id("engine").Dot("Tree").Dot(Title(configType.Name)).Index(Id(Title(configType.Name)+"ID").Call(Id("p").Index(Lit(0)).Dot("ID"))),
 						If(Id("!ok")).Block(
-							Id("child").Op("=").Id(configType.Name).Values(Dict{Id("ID"): Id(Title(configType.Name) + "ID").Call(Id("p").Index(Lit(0)).Dot("id"))}),
+							Id("child").Op("=").Id(configType.Name).Values(Dict{Id("ID"): Id(Title(configType.Name) + "ID").Call(Id("p").Index(Lit(0)).Dot("ID"))}),
 						),
 						Id("engine").Dot("assemble"+Title(configType.Name)+"Path").Params(Id("&child"), Id("p"), Lit(0), Id("engine").Dot("planner").Dot("includedElements")),
-						Id("engine").Dot("Tree").Dot(Title(configType.Name)).Index(Id(Title(configType.Name)+"ID").Call(Id("p").Index(Lit(0)).Dot("id"))).Op("=").Id("child"),
+						Id("engine").Dot("Tree").Dot(Title(configType.Name)).Index(Id(Title(configType.Name)+"ID").Call(Id("p").Index(Lit(0)).Dot("ID"))).Op("=").Id("child"),
 					)
 				}),
 			),
