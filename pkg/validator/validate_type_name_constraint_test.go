6e76cc66a3b55156cfee67ec5cc0449aca13db28
Jonas Bergner <jonas.bergner96@gmail.com>
2022-05-06 17:01:34 +0200

    feat: disallow query prefix for types

--- pkg/validator/validate_type_name_constraint_test.go
+++ pkg/validator/validate_type_name_constraint_test.go
@@ -9,12 +9,15 @@ import (
 func TestValidateTypeNameConstraint(t *testing.T) {
 	t.Run("should fail on violating type names", func(t *testing.T) {
 		data := map[interface{}]interface{}{
-			"fooID":   map[interface{}]interface{}{},
-			"bar_baz": map[interface{}]interface{}{},
+			"queryBan": map[interface{}]interface{}{},
+			"foo":      map[interface{}]interface{}{},
+			"fooID":    map[interface{}]interface{}{},
+			"bar_baz":  map[interface{}]interface{}{},
 		}
 
 		actualErrors := validateTypeNameConstraintViolation(data)
 		expectedErrors := []error{
+			newValidationErrorTypeNameConstraintViolation("queryBan"),
 			newValidationErrorTypeNameConstraintViolation("fooID"),
 			newValidationErrorTypeNameConstraintViolation("bar_baz"),
 		}
